5f977a03724fe9c57d478050267249a5ce25d480
Stephan Dinter <st-di@gmx.de>
2020-10-15 16:08:11 +0200

    remove clientside timestamp -> simply use time on server, rename some files

--- lib/models/user_position.dart
+++ lib/models/user_position.dart
@@ -3,16 +3,14 @@ class UserPosition {
   double lon;
   int speed;
   String id;
-  int timestamp;
 
-  UserPosition({this.lat, this.lon, this.speed, this.id, this.timestamp});
+  UserPosition({this.lat, this.lon, this.speed, this.id});
 
   UserPosition.fromJson(Map<String, dynamic> json) {
     lat = json['lat'];
     lon = json['lon'];
     speed = json['speed'];
     id = json['id'];
-    timestamp = json['timestamp'];
   }
 
   Map<String, dynamic> toJson() {
@@ -21,7 +19,6 @@ class UserPosition {
     data['lon'] = this.lon;
     data['speed'] = this.speed;
     data['id'] = this.id;
-    data['timestamp'] = this.timestamp;
     return data;
   }
 }
--- lib/services/app_service.dart
+++ lib/services/app_service.dart
@@ -4,16 +4,15 @@ import 'dart:convert';
 import 'package:bikenow/config/logger.dart';
 import 'package:bikenow/models/api/api_route.dart';
 import 'package:bikenow/models/message.dart';
-import 'package:bikenow/models/stopRequest.dart';
+import 'package:bikenow/models/stop_request.dart';
 import 'package:bikenow/models/user_position.dart';
 import 'package:bikenow/models/recommendation.dart';
-import 'package:bikenow/models/routeRequest.dart';
+import 'package:bikenow/models/route_request.dart';
 import 'package:bikenow/services/mqtt_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:uuid/uuid.dart';
-import 'package:ntp/ntp.dart';
 
 class AppService with ChangeNotifier {
   Logger log = Logger("AppService");
@@ -41,12 +40,12 @@ class AppService with ChangeNotifier {
     _mqttService.messageStreamController.stream.listen((message) {
       if (message.topic.contains('resroute')) {
         route = ApiRoute.fromJson(json.decode(message.payload));
-        log.i('-> Route');
+        log.i('<- Route');
       }
 
       if (message.topic.contains('recommendation')) {
         recommendation = Recommendation.fromJson(json.decode(message.payload));
-        log.i('-> Recommendation');
+        log.i('<- Recommendation');
       }
 
       // log.i('New Message! topic: ${message.topic}, payload: ${message.payload}');
@@ -83,21 +82,10 @@ class AppService with ChangeNotifier {
 
     _mqttService.publish(routeRequest);
 
-    log.i('<- Route Request');
+    log.i('-> Route Request');
   }
 
   startGeolocation() async {
-    DateTime localTime = DateTime.now().toLocal();
-
-    log.i('Get time offset ...');
-
-    int offset = await NTP.getNtpOffset(
-      lookUpAddress: 'time.zih.tu-dresden.de',
-      localTime: localTime,
-    );
-
-    log.i('NTP time offset: $offset ms');
-
     isGeolocating = true;
 
     positionStream = getPositionStream(
@@ -114,15 +102,12 @@ class AppService with ChangeNotifier {
               lat: position.latitude,
               lon: position.longitude,
               speed: (position.speed * 3.6).round(),
-              timestamp: localTime
-                  .add(new Duration(milliseconds: offset))
-                  .millisecondsSinceEpoch,
             ).toJson(),
           ),
         );
 
         _mqttService.publish(positionMessage);
-        log.i('<- Position');
+        log.i('-> Position');
       }
     });
     log.i('GEOLOCATOR STARTED!');
@@ -139,7 +124,7 @@ class AppService with ChangeNotifier {
     );
 
     _mqttService.publish(stopRequest);
-    log.i('<- Stop Request');
+    log.i('-> Stop Request');
 
     isGeolocating = false;
     positionStream.cancel();
