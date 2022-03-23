381dc4f5ff6d183bc16813c3c9075e2a69734406
Philipp Matthes <philipp.matthes@tu-dresden.de>
2022-03-23 10:39:37 +0100

    Add location usage request and point to settings if not granted

--- lib/services/app.dart
+++ lib/services/app.dart
@@ -12,6 +12,8 @@ import 'package:priobike/utils/logger.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
 
+import 'navigation.dart';
+
 class AppService with ChangeNotifier {
   Logger log = Logger("AppService");
 
@@ -69,8 +71,88 @@ class AppService with ChangeNotifier {
     notifyListeners();
   }
 
+  showLocationAccessDeniedDialog(BuildContext context) {
+    Widget okButton = TextButton(
+      child: const Text("Einstellungen öffnen"),
+      onPressed: () {
+        Geolocator.openLocationSettings();
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: const Text("Zugriff auf Standort verweigert."),
+      content: const Text("Bitte erlauben Sie den Zugriff auf Ihren Standort in den Einstellungen."),
+      actions: [
+        okButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  Future<bool> requestGeolocatorPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled - don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return false;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return false;
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return true;
+  }
+
   startGeolocation() async {
+    if (isGeolocating) {
+      log.w('Attempted to start geolocation while already geolocating');
+      return;
+    }
     isGeolocating = true;
+
+    final hasPermission = await requestGeolocatorPermission();
+    if (!hasPermission) {
+      final context = NavigationService.key.currentContext;
+      if (context == null) {
+        log.e('Cannot show alert dialog, no context');
+        return;
+      }
+      // Pop to the root of the navigator stack before directing to the settings.
+      Navigator.of(context).popUntil((route) => route.isFirst);
+      showLocationAccessDeniedDialog(context);
+      log.w('Permission to Geolocator denied');
+      isGeolocating = false;
+      return;
+    }
+
     loadingRecommendation = true;
     positionStream = Geolocator.getPositionStream(
       locationSettings: const LocationSettings(
