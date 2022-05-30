bf6b3b937bafb8a772acbf27a92b1f09706bbca6
stephdin <stephdin@users.noreply.github.com>
2022-05-30 16:49:46 +0200

    chore: moved arrow images to assets subfolder

--- lib/widgets/navigation_arrow.dart
+++ lib/widgets/navigation_arrow.dart
@@ -18,62 +18,62 @@ class NavigationArrow extends StatelessWidget {
     switch (sign) {
       case -7:
         return Image.asset(
-          'images/dark_keep_left.png',
+          'assets/images/dark_keep_left.png',
           width: width,
         );
       case -3:
         return Image.asset(
-          'images/dark_turn_sharp_left.png',
+          'assets/images/dark_turn_sharp_left.png',
           width: width,
         );
       case -2:
         return Image.asset(
-          'images/dark_turn_left.png',
+          'assets/images/dark_turn_left.png',
           width: width,
         );
       case -1:
         return Image.asset(
-          'images/dark_turn_slight_left.png',
+          'assets/images/dark_turn_slight_left.png',
           width: width,
         );
       case 0:
         return Image.asset(
-          'images/dark_continue.png',
+          'assets/images/dark_continue.png',
           width: width,
         );
       case 1:
         return Image.asset(
-          'images/dark_turn_slight_right.png',
+          'assets/images/dark_turn_slight_right.png',
           width: width,
         );
       case 2:
         return Image.asset(
-          'images/dark_turn_right.png',
+          'assets/images/dark_turn_right.png',
           width: width,
         );
       case 3:
         return Image.asset(
-          'images/dark_turn_sharp_right.png',
+          'assets/images/dark_turn_sharp_right.png',
           width: width,
         );
       case 7:
         return Image.asset(
-          'images/dark_keep_right.png',
+          'assets/images/dark_keep_right.png',
           width: width,
         );
       case 4:
         return Image.asset(
-          'images/dark_finish.png',
+          'assets/images/dark_finish.png',
           width: width,
         );
       case 6:
         return Image.asset(
-          'images/dark_roundabout.png',
+          'assets/images/dark_roundabout.png',
           width: width,
         );
       default:
         return Image.asset(
-          'images/dark_continue.png',
+          'assets/images/dark_continue.png',
           width: width,
         );
     }
