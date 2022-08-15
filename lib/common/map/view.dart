0bf073431a7dab65d6420ce30e58670946535835
Philipp Matthes <philipp.matthes@tu-dresden.de>
2022-08-15 14:18:00 +0200

    Refactor AppMap to configure map center by settings

--- lib/common/map/view.dart
+++ lib/common/map/view.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:priobike/settings/models/backend.dart';
+import 'package:priobike/settings/service.dart';
+import 'package:provider/provider.dart';
 
-class AppMap extends MapboxMap {
+class AppMap extends StatefulWidget {
+  /// A callback that is executed when the map was created.
+  final void Function(MapboxMapController)? onMapCreated;
+
+  /// A callback that is executed when the style was loaded.
+  final void Function()? onStyleLoaded;
+
   const AppMap({
-    Key? key,
-    CameraPosition initialCameraPosition = const CameraPosition(
-      target: LatLng(53.551086, 9.993682), // Hamburg
-      tilt: 0,
-      zoom: 10
-    ),
-    void Function(MapboxMapController)? onMapCreated,
-    void Function()? onStyleLoaded,
-    AttributionButtonPosition? attributionButtonPosition = AttributionButtonPosition.BottomRight,
-  }) : super(
-    key: key,
-    styleString: "mapbox://styles/mapbox/light-v10",
-    // TODO: Pass this access token by environment
-    accessToken: "pk.eyJ1Ijoic25ybXR0aHMiLCJhIjoiY2w0ZWVlcWt5MDAwZjNjbW5nMHNvN3kwNiJ9.upoSvMqKIFe3V_zPt1KxmA",
-    onMapCreated: onMapCreated,
-    onStyleLoadedCallback: onStyleLoaded,
-    attributionButtonPosition: AttributionButtonPosition.BottomRight,
-    initialCameraPosition: initialCameraPosition,
-  );
+    this.onMapCreated,
+    this.onStyleLoaded,
+    Key? key
+  }) : super(key: key);
+
+  @override 
+  AppMapState createState() => AppMapState();
+}
+
+class AppMapState extends State<AppMap> {
+  /// The associated settings service, which is injected by the provider.
+  late SettingsService settingsService;
+
+  @override
+  void didChangeDependencies() {
+    settingsService = Provider.of<SettingsService>(context);
+    super.didChangeDependencies();
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return MapboxMap(
+      styleString: "mapbox://styles/mapbox/light-v10",
+      accessToken: "pk.eyJ1Ijoic25ybXR0aHMiLCJhIjoiY2w0ZWVlcWt5MDAwZjNjbW5nMHNvN3kwNiJ9.upoSvMqKIFe3V_zPt1KxmA",
+      onMapCreated: widget.onMapCreated,
+      onStyleLoadedCallback: widget.onStyleLoaded,
+      attributionButtonPosition: AttributionButtonPosition.BottomRight,
+      initialCameraPosition: CameraPosition(
+        target: settingsService.backend.center,
+        tilt: 0,
+        zoom: 11
+      ),
+    );
+  }
 }
