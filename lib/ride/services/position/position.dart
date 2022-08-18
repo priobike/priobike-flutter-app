3e1e801eb1e25526353bcfbc7a27a042069cff88
Philipp Matthes <philipp.matthes@tu-dresden.de>
2022-08-18 15:05:22 +0200

    Fix GNSS positioning service

--- lib/ride/services/position/position.dart
+++ lib/ride/services/position/position.dart
@@ -30,7 +30,6 @@ class PositionExtrapolator {
     timer = Timer.periodic(Duration(milliseconds: (1000 / refreshRateHz).round()), (Timer timer) {
       if (lastPosition == null) return;
       var extrapolatedPosition = extrapolate(lastPosition!);
-      if (extrapolatedPosition == null) return;
       streamController.add(extrapolatedPosition);
     });
 
@@ -42,19 +41,15 @@ class PositionExtrapolator {
   }
 
   /// Extrapolate the position.
-  Position? extrapolate(Position lastPosition) {
+  Position extrapolate(Position lastPosition) {
     // Calculate the elapsed time since the last position.
     final last = lastPosition.timestamp?.toUtc();
-    if (last == null) return null;
+    if (last == null) return lastPosition;
     final elapsed = DateTime.now().toUtc().difference(last).inMilliseconds;
-    if (elapsed < 0) return null;
+    if (elapsed < 0) return lastPosition;
 
     // Offset the position by the traveled distance and bearing
-    if (lastPosition.heading < 0 || lastPosition.heading > 360) return null;
-    // Unfortunately, if the heading is 0.0, we need to assume that 
-    // the device has no support for heading. In the future, we should
-    // fallback to calculating the heading ourselves (using last 2 positions).
-    if (lastPosition.heading == 0.0) return null;
+    if (lastPosition.heading < 0 || lastPosition.heading > 360) return lastPosition;
     final newLatLng = const Distance().offset(
       LatLng(lastPosition.latitude, lastPosition.longitude), 
       lastPosition.speed * (elapsed / 1000), 
