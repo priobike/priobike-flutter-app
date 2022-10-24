4be54567a56a16ca2dc98221c02df2732125f86c
PaulPickhardt <s2137275@msx.tu-dresden.de>
2022-10-24 18:35:03 +0200

    Add shortcuts to routes view

--- lib/routing/views/routes.dart
+++ lib/routing/views/routes.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Shortcuts;
 import 'package:flutter/services.dart';
 import 'package:priobike/common/layout/buttons.dart';
 import 'package:priobike/common/layout/text.dart';
-import 'package:priobike/home/services/profile.dart';
+import 'package:priobike/home/models/shortcut.dart';
+import 'package:priobike/home/services/shortcuts.dart';
+import 'package:priobike/routing/models/waypoint.dart';
 import 'package:provider/provider.dart';
 
 class RoutesView extends StatefulWidget {
@@ -14,7 +16,7 @@ class RoutesView extends StatefulWidget {
 
 class RoutesViewState extends State<RoutesView> {
   /// The associated shortcuts service, which is injected by the provider.
-  Profile? profileService;
+  late Shortcuts shortcuts;
 
   @override
   void initState() {
@@ -23,14 +25,81 @@ class RoutesViewState extends State<RoutesView> {
 
   @override
   void didChangeDependencies() {
-    profileService = Provider.of<Profile>(context);
+    shortcuts = Provider.of<Shortcuts>(context);
 
     super.didChangeDependencies();
+  }
+
+  Widget _waypointItem(Waypoint waypoint) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Content(
+          context: context,
+          text: waypoint.address ?? "Keine Addresse vorhanden",
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  Widget _itemDivider() {
+    return const Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Icon(Icons.more_vert, color: Colors.grey),
+      ),
+    );
+  }
+
+  Widget _shortcutRowItem(Shortcut shortcut) {
+    List<Widget> waypoints =
+        shortcut.waypoints.map((entry) => _waypointItem(entry)).toList();
+
+    for (int i = 1; i < waypoints.length; i += 2) {
+      waypoints.insert(i, _itemDivider());
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 0, left: 20, bottom: 20, right: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: BoldSubHeader(
+                  text: shortcut.name,
+                  context: context,
+                  maxLines: 3,
+                ),
+              ),
+              const SizedBox(width: 5),
+              SmallIconButton(
+                icon: Icons.delete,
+                onPressed: () {
+                  print("DEELTE");
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          ...waypoints
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final frame = MediaQuery.of(context);
+    List<Widget> shortcutsList = [];
+    if (shortcuts.shortcuts != null) {
+      shortcutsList =
+          shortcuts.shortcuts!.map((entry) => _shortcutRowItem(entry)).toList();
+    }
 
     return AnnotatedRegion<SystemUiOverlayStyle>(
       // Show status bar in opposite color of the background.
@@ -70,13 +139,9 @@ class RoutesViewState extends State<RoutesView> {
                   const SizedBox(width: 80),
                 ]),
                 const SizedBox(height: 20),
-
-                /// height = height - 20 - 88 - 20 - view.inset.top (padding, backButton, offset)
-                SizedBox(
-                  width: frame.size.width,
-                  height: frame.size.height - 128,
+                Expanded(
                   child: ListView(
-                    children: [],
+                    children: shortcutsList,
                   ),
                 ),
               ],
