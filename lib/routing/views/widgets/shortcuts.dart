6d7b5480aa517c0e22553d010af30dc071d0d7a6
PaulPickhardt <s2137275@msx.tu-dresden.de>
2022-10-27 11:40:36 +0200

    Add places to shortcuts

--- lib/routing/views/widgets/shortcuts.dart
+++ lib/routing/views/widgets/shortcuts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' hide Shortcuts;
 import 'package:priobike/common/layout/text.dart';
+import 'package:priobike/home/services/places.dart';
 import 'package:priobike/home/services/shortcuts.dart';
 import 'package:priobike/routing/services/routing.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +20,9 @@ class ShortCutsRowState extends State<ShortCutsRow> {
   /// The associated shortcut service, which is injected by the provider.
   late Shortcuts shortcuts;
 
+  /// The associated places service, which is injected by the provider.
+  late Places places;
+
   /// The associated routing service, which is injected by the provider.
   late Routing routing;
 
@@ -26,6 +30,7 @@ class ShortCutsRowState extends State<ShortCutsRow> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     shortcuts = Provider.of<Shortcuts>(context);
+    places = Provider.of<Places>(context);
     routing = Provider.of<Routing>(context);
   }
 
@@ -43,7 +48,7 @@ class ShortCutsRowState extends State<ShortCutsRow> {
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
             child: Row(
               children: [
-                Icon(isShortCut ? Icons.alt_route : null),
+                Icon(isShortCut ? Icons.alt_route : Icons.home),
                 const SizedBox(width: 5),
                 Content(text: name, context: context),
               ],
@@ -62,12 +67,18 @@ class ShortCutsRowState extends State<ShortCutsRow> {
         if (widget.close) Navigator.of(context).pop();
       }, true);
     }).toList();
+    final List<Widget> placesItems = places.places!.map((entry) {
+      return _shortcutItem(context, entry.name, () async {
+        widget.onPressed([entry.waypoint]);
+        if (widget.close) Navigator.of(context).pop();
+      }, false);
+    }).toList();
     return SizedBox(
       /// 32 + 2*10 padding
       height: 52,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: shortCutItems,
+        children: [...placesItems, ...shortCutItems],
       ),
     );
   }
