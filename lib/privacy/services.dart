priobike/priobike-flutter-app

commit 2e69ac6fa998a344be115472c743463206e635a7

Bugfix Privacynotice on Android showed every start (#41)
* format views.dart for privacy

* renamed privacyService variable

* made code easier to read

* improved readablity of code

* privacy policy bug for android was solved by .trim()

* removed logging

* changed storedPrivacyPolicy from late initialisation to initialisator with default value

* made default values nullable again

* Polishing, add debug feature to delete stored privacy policy

Co-authored-by: Philipp Matthes <philipp.matthes@tu-dresden.de>

lib/privacy/services.dart
@@ -5,10 +5,10 @@ class PrivacyPolicy with ChangeNotifier {
   /// The key under which the accepted privacy policy is stored in the user defaults / shared preferences.
   static const key = "priobike.privacy.accepted-policy";
 
-  var hasLoaded = false;
+  bool hasLoaded = false;
 
   /// The text of the privacy policy.
-  String? text;
+  String? storedPrivacyPolicy;
 
   /// An indicator if the privacy policy was confirmed by the user.
   bool? isConfirmed;
@@ -19,23 +19,34 @@ class PrivacyPolicy with ChangeNotifier {
   /// Load the privacy policy.
   Future<void> loadPolicy(BuildContext context) async {
     if (hasLoaded) return;
-    text = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+
     final storage = await SharedPreferences.getInstance();
-    final accepted = storage.getString(key);
-    isConfirmed = accepted == text;
-    hasChanged = accepted != null && !isConfirmed!;
+    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+    storedPrivacyPolicy = storage.getString(key) ?? '';
+
+    // Strings must be have their leading and tailing whitespaces trimmed
+    // otherwise Android will have a bug where equals versions of the privacy notice are not equal.
+    isConfirmed = (newPrivacyPolicy.trim() == storedPrivacyPolicy?.trim());
+    hasChanged = !isConfirmed!;
     hasLoaded = true;
 
     notifyListeners();
   }
 
-  /// Confirm the privacy policy.
-  Future<void> confirm() async {
+  /// Delete the stored privacy policy for debugging purposes.
+  Future<void> deleteStoredPolicy() async {
+    final storage = await SharedPreferences.getInstance();
+    await storage.remove(key);
+    notifyListeners();
+  }
+
+  /// Confirm the privacy policy and commits the new version to shared preferences.
+  Future<void> confirm(BuildContext context) async {
     if (!hasLoaded) return;
     final storage = await SharedPreferences.getInstance();
-    await storage.setString(key, text!);
-    isConfirmed = true;
+    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
 
+    isConfirmed = await storage.setString(key, newPrivacyPolicy);
     notifyListeners();
   }
 }

lib/privacy/views.dart
@@ -9,8 +9,10 @@ import 'package:provider/provider.dart';
 /// A list item with icon.
 class IconItem extends Row {
   IconItem({Key? key, required IconData icon, required String text, required BuildContext context})
-      : super(key: key, children: [
-          SizedBox(
+      : super(
+          key: key,
+          children: [
+            SizedBox(
               width: 64,
               height: 64,
               child: Icon(
@@ -18,10 +20,14 @@ class IconItem extends Row {
                 color: Colors.blueAccent,
                 size: 64,
                 semanticLabel: text,
-              )),
-          const SmallHSpace(),
-          Expanded(child: Content(text: text, context: context)),
-        ]);
+              ),
+            ),
+            const SmallHSpace(),
+            Expanded(
+              child: Content(text: text, context: context),
+            ),
+          ],
+        );
 }
 
 /// A view that displays the privacy policy.
@@ -37,45 +43,31 @@ class PrivacyPolicyView extends StatefulWidget {
 
 class PrivacyPolicyViewState extends State<PrivacyPolicyView> {
   /// The associated privacy service, which is injected by the provider.
-  late PrivacyPolicy s;
+  late PrivacyPolicy privacyService;
 
   @override
   void didChangeDependencies() {
-    s = Provider.of<PrivacyPolicy>(context);
+    privacyService = Provider.of<PrivacyPolicy>(context);
 
     // Load once the window was built.
-    WidgetsBinding.instance?.addPostFrameCallback((_) async {
-      await s.loadPolicy(context);
-    });
-
-    super.didChangeDependencies();
-  }
-
-  /// Render a loading indicator.
-  Widget renderLoadingIndicator() {
-    return Scaffold(
-      body: Container(
-        color: Theme.of(context).colorScheme.background,
-        width: MediaQuery.of(context).size.width,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
-          CircularProgressIndicator(),
-          SizedBox(height: 16),
-          Text("Lade...", style: TextStyle(fontSize: 16)),
-        ]),
-      ),
+    WidgetsBinding.instance?.addPostFrameCallback(
+      (_) async {
+        await privacyService.loadPolicy(context);
+      },
     );
+    super.didChangeDependencies();
   }
 
   /// A callback that is executed when the accept button was pressed.
   Future<void> onAcceptButtonPressed() async {
-    await s.confirm();
+    await privacyService.confirm(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!s.hasLoaded) return Container();
+    if (!privacyService.hasLoaded) return Container();
 
-    if (s.isConfirmed == true && widget.child != null) return widget.child!;
+    if ((privacyService.isConfirmed == true) && (widget.child != null)) return widget.child!;
 
     return Scaffold(
       body: Container(
@@ -90,18 +82,19 @@ class PrivacyPolicyViewState extends State<PrivacyPolicyView> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const SizedBox(height: 164),
-                      if (!s.hasChanged!) Header(text: "Diese App funktioniert mit", context: context),
-                      if (!s.hasChanged!) Header(text: "deinen Daten.", color: Colors.blueAccent, context: context),
-                      if (s.hasChanged!) Header(text: "Wir haben die Erklärung zum", context: context),
-                      if (s.hasChanged!)
+                      if (!privacyService.hasChanged!) Header(text: "Diese App funktioniert mit", context: context),
+                      if (!privacyService.hasChanged!)
+                        Header(text: "deinen Daten.", color: Colors.blueAccent, context: context),
+                      if (privacyService.hasChanged!) Header(text: "Wir haben die Erklärung zum", context: context),
+                      if (privacyService.hasChanged!)
                         Header(text: "Datenschutz aktualisiert.", color: Colors.blueAccent, context: context),
                       const SmallVSpace(),
-                      if (!s.hasChanged!)
+                      if (!privacyService.hasChanged!)
                         SubHeader(
                             text:
                                 "Bitte lies dir deshalb kurz durch, wie wir deine Daten schützen. Das Wichtigste zuerst:",
                             context: context),
-                      if (s.hasChanged!)
+                      if (privacyService.hasChanged!)
                         SubHeader(text: "Lies dir hierzu kurz unsere Änderungen durch.", context: context),
                       const VSpace(),
                       IconItem(
@@ -122,7 +115,7 @@ class PrivacyPolicyViewState extends State<PrivacyPolicyView> {
                               "Um die App zu verbessern, sammeln wir Informationen über den Komfort von Straßen, Fehlerberichte und Feedback.",
                           context: context),
                       const VSpace(),
-                      Content(text: s.text!, context: context),
+                      Content(text: privacyService.storedPrivacyPolicy!, context: context),
                       const SizedBox(height: 256),
                     ],
                   ),
@@ -131,12 +124,17 @@ class PrivacyPolicyViewState extends State<PrivacyPolicyView> {
             ),
             if (widget.child == null)
               SafeArea(
-                  child: Column(children: [
-                const SizedBox(height: 8),
-                Row(children: [
-                  AppBackButton(onPressed: () => Navigator.pop(context)),
-                ]),
-              ])),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        AppBackButton(onPressed: () => Navigator.pop(context)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             if (widget.child != null)
               Pad(
                 child: BigButton(

lib/settings/views/main.dart
@@ -8,6 +8,7 @@ import 'package:priobike/common/layout/tiles.dart';
 import 'package:priobike/feedback/views/main.dart';
 import 'package:priobike/home/services/shortcuts.dart';
 import 'package:priobike/licenses/views.dart';
+import 'package:priobike/privacy/services.dart';
 import 'package:priobike/settings/models/datastream.dart';
 import 'package:priobike/settings/models/routing.dart';
 import 'package:priobike/settings/models/speed.dart';
@@ -407,6 +408,15 @@ class SettingsViewState extends State<SettingsView> {
                           title: "Tutorials zurücksetzen",
                           icon: Icons.recycling,
                           callback: () => Provider.of<Tutorial>(context, listen: false).deleteCompleted(),
+                        ),
+                      ),
+                    if (settings.enableInternalFeatures)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: SettingsElement(
+                          title: "Datenschutz zurücksetzen",
+                          icon: Icons.recycling,
+                          callback: () => Provider.of<PrivacyPolicy>(context, listen: false).deleteStoredPolicy(),
                         ),
                       ),
                     if (feature.canEnableBetaFeatures)

commit 2820b7ca645d0ded75732b88c6a39bae72d994c9

Fix bugs that were introduced in 2e69ac6 and polish variable naming

lib/privacy/services.dart
@@ -8,7 +8,7 @@ class PrivacyPolicy with ChangeNotifier {
   bool hasLoaded = false;
 
   /// The text of the privacy policy.
-  String? storedPrivacyPolicy;
+  String? assetText;
 
   /// An indicator if the privacy policy was confirmed by the user.
   bool? isConfirmed;
@@ -21,15 +21,14 @@ class PrivacyPolicy with ChangeNotifier {
     if (hasLoaded) return;
 
     final storage = await SharedPreferences.getInstance();
-    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
-    storedPrivacyPolicy = storage.getString(key) ?? '';
+    assetText = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+    final storedPrivacyPolicy = storage.getString(key);
 
     // Strings must be have their leading and tailing whitespaces trimmed
     // otherwise Android will have a bug where equals versions of the privacy notice are not equal.
-    isConfirmed = (newPrivacyPolicy.trim() == storedPrivacyPolicy?.trim());
+    isConfirmed = storedPrivacyPolicy?.trim() == assetText?.trim();
     hasChanged = !isConfirmed!;
     hasLoaded = true;
-
     notifyListeners();
   }
 
@@ -37,6 +36,8 @@ class PrivacyPolicy with ChangeNotifier {
   Future<void> deleteStoredPolicy() async {
     final storage = await SharedPreferences.getInstance();
     await storage.remove(key);
+    isConfirmed = false;
+    hasChanged = true;
     notifyListeners();
   }
 
@@ -44,9 +45,10 @@ class PrivacyPolicy with ChangeNotifier {
   Future<void> confirm(BuildContext context) async {
     if (!hasLoaded) return;
     final storage = await SharedPreferences.getInstance();
-    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+    final confirmedPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
 
-    isConfirmed = await storage.setString(key, newPrivacyPolicy);
+    isConfirmed = await storage.setString(key, confirmedPolicy);
+    hasChanged = false;
     notifyListeners();
   }
 }

lib/privacy/views.dart
@@ -115,7 +115,7 @@ class PrivacyPolicyViewState extends State<PrivacyPolicyView> {
                               "Um die App zu verbessern, sammeln wir Informationen über den Komfort von Straßen, Fehlerberichte und Feedback.",
                           context: context),
                       const VSpace(),
-                      Content(text: privacyService.storedPrivacyPolicy!, context: context),
+                      Content(text: privacyService.assetText!, context: context),
                       const SizedBox(height: 256),
                     ],
                   ),