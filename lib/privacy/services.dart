2e69ac6fa998a344be115472c743463206e635a7
Charles Krüger <89537082+kruegercharles@users.noreply.github.com>
2022-11-15 17:33:12 +0100

    Bugfix Privacynotice on Android showed every start (#41)
    * format views.dart for privacy
    
    * renamed privacyService variable
    
    * made code easier to read
    
    * improved readablity of code
    
    * privacy policy bug for android was solved by .trim()
    
    * removed logging
    
    * changed storedPrivacyPolicy from late initialisation to initialisator with default value
    
    * made default values nullable again
    
    * Polishing, add debug feature to delete stored privacy policy
    
    Co-authored-by: Philipp Matthes <philipp.matthes@tu-dresden.de>

--- lib/privacy/services.dart
+++ lib/privacy/services.dart
@@ -5,10 +5,10 @@ class PrivacyPolicy with ChangeNotifier {
   /// The key under which the accepted privacy policy is stored in the user defaults / shared preferences.
   static const key = "priobike.privacy.accepted-policy";
 
-  var hasLoaded = false;
+  bool hasLoaded = false;
 
   /// The text of the privacy policy.
-  String? text;
+  String? storedPrivacyPolicy;
 
   /// An indicator if the privacy policy was confirmed by the user.
   bool? isConfirmed;
@@ -19,23 +19,34 @@ class PrivacyPolicy with ChangeNotifier {
   /// Load the privacy policy.
   Future<void> loadPolicy(BuildContext context) async {
     if (hasLoaded) return;
-    text = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+
     final storage = await SharedPreferences.getInstance();
-    final accepted = storage.getString(key);
-    isConfirmed = accepted == text;
-    hasChanged = accepted != null && !isConfirmed!;
+    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
+    storedPrivacyPolicy = storage.getString(key) ?? '';
+
+    // Strings must be have their leading and tailing whitespaces trimmed
+    // otherwise Android will have a bug where equals versions of the privacy notice are not equal.
+    isConfirmed = (newPrivacyPolicy.trim() == storedPrivacyPolicy?.trim());
+    hasChanged = !isConfirmed!;
     hasLoaded = true;
 
     notifyListeners();
   }
 
-  /// Confirm the privacy policy.
-  Future<void> confirm() async {
+  /// Delete the stored privacy policy for debugging purposes.
+  Future<void> deleteStoredPolicy() async {
+    final storage = await SharedPreferences.getInstance();
+    await storage.remove(key);
+    notifyListeners();
+  }
+
+  /// Confirm the privacy policy and commits the new version to shared preferences.
+  Future<void> confirm(BuildContext context) async {
     if (!hasLoaded) return;
     final storage = await SharedPreferences.getInstance();
-    await storage.setString(key, text!);
-    isConfirmed = true;
+    final String newPrivacyPolicy = await DefaultAssetBundle.of(context).loadString("assets/text/privacy.txt");
 
+    isConfirmed = await storage.setString(key, newPrivacyPolicy);
     notifyListeners();
   }
 }
