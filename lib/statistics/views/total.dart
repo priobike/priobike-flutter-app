572bf22d069f253004132326bcdee5aa7dafc0e5
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2022-12-13 15:49:10 +0100

    refactore statistics/views/total

--- lib/statistics/views/total.dart
+++ lib/statistics/views/total.dart
@@ -67,6 +67,46 @@ class TotalStatisticsViewState extends State<TotalStatisticsView> {
     super.didChangeDependencies();
   }
 
+  Widget renderCo2DialogBox() {
+    return AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(24)),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background.withOpacity(0.95),
+      title: BoldContent(text: "Information zur CO2-Berechnung", context: context),
+      content: Content(
+          text:
+              "Bei diesem Wert handelt es sich um eine Schätzung anhand deiner gefahrenen Distanz und einem durchschnittlichen CO2-Ausstoß von 118,7 g/km (Daten: Statista.com, 2021). Der tatsächliche CO2-Ausstoß kann je nach Fahrzeug und Fahrweise abweichen.",
+          context: context),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text("Alles klar"),
+        ),
+      ],
+    );
+  }
+
+  Widget renderInfoDialogBox() {
+    return AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(24)),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background.withOpacity(0.95),
+      title: BoldContent(text: "Fahrtstatistiken", context: context),
+      content: Content(
+          text:
+              "Die gezeigten Fahrtstatistiken werden nur auf diesem Gerät gespeichert. Sie werden nicht an einen Server gesendet.",
+          context: context),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text("Alles klar"),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -87,23 +127,7 @@ class TotalStatisticsViewState extends State<TotalStatisticsView> {
               splash: Colors.white,
               onPressed: () => showDialog(
                 context: context,
-                builder: (context) => AlertDialog(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(24)),
-                  ),
-                  backgroundColor: Theme.of(context).colorScheme.background.withOpacity(0.95),
-                  title: BoldContent(text: "Fahrtstatistiken", context: context),
-                  content: Content(
-                      text:
-                          "Die gezeigten Fahrtstatistiken werden nur auf diesem Gerät gespeichert. Sie werden nicht an einen Server gesendet.",
-                      context: context),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text("Alles klar"),
-                    ),
-                  ],
-                ),
+                builder: (context) => renderInfoDialogBox(),
               ),
             ),
             const SizedBox(width: 40),
@@ -117,42 +141,31 @@ class TotalStatisticsViewState extends State<TotalStatisticsView> {
               const Divider(),
               StatisticsElementView(
                 icon: Icons.co2_rounded,
-                title: "${(statistics.totalSavedCO2Kg)?.toStringAsFixed(1) ?? 0} kg",
+                title: (statistics.totalSavedCO2Kg ?? 0) < 1
+                    ? "${(statistics.totalSavedCO2Kg ?? 0) * 1000} g"
+                    : "${(statistics.totalSavedCO2Kg)?.toStringAsFixed(1) ?? 0} kg",
                 subtitle: "CO2 eingespart",
                 context: context,
                 onPressed: () => showDialog(
                   context: context,
-                  builder: (context) => AlertDialog(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(24)),
-                    ),
-                    backgroundColor: Theme.of(context).colorScheme.background.withOpacity(0.95),
-                    title: BoldContent(text: "Information zur CO2-Berechnung", context: context),
-                    content: Content(
-                        text:
-                            "Bei diesem Wert handelt es sich um eine Schätzung anhand deiner gefahrenen Distanz und einem durchschnittlichen CO2-Ausstoß von 118,7 g/km (Daten: Statista.com, 2021). Der tatsächliche CO2-Ausstoß kann je nach Fahrzeug und Fahrweise abweichen.",
-                        context: context),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(),
-                        child: const Text("Alles klar"),
-                      ),
-                    ],
-                  ),
+                  builder: (context) => renderCo2DialogBox(),
                 ),
               ),
               const Divider(),
               StatisticsElementView(
                 icon: Icons.directions_bike_rounded,
-                title: "${((statistics.totalDistanceMeters ?? 0) / 1000).toStringAsFixed(2)} km",
+                title: (statistics.totalDistanceMeters ?? 0) >= 1000
+                    ? "${((statistics.totalDistanceMeters ?? 0) / 1000).toStringAsFixed(2)} km"
+                    : "${(statistics.totalDistanceMeters ?? 0).toStringAsFixed(0)} m",
                 subtitle: "Gefahrene Distanz",
                 context: context,
               ),
               const Divider(),
               StatisticsElementView(
                 icon: Icons.timer_outlined,
-                title:
-                    "${Duration(seconds: (statistics.totalDurationSeconds ?? 0.0).toInt()).toString().split('.').first} Std.",
+                title: (statistics.totalDurationSeconds ?? 0.0) >= 3600
+                    ? "${Duration(seconds: (statistics.totalDurationSeconds ?? 0.0).toInt()).toString().split('.').first} Std."
+                    : "${((statistics.totalDurationSeconds ?? 0) / 60).toStringAsFixed(0)} Min.",
                 subtitle: "Gefahrene Zeit",
                 context: context,
               ),
