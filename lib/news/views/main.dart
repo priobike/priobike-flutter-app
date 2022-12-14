aeb76ce630dd01bfa4269ab20d645404ef0af989
Daniel <dajesch@gmail.com>
2022-12-14 11:49:37 +0100

    Remove sliver app bar from news, settings and edit view (#85)
    * Remove sliver app bar from news, settings and edit view
    * Remove unnecessary container from news view

--- lib/news/views/main.dart
+++ lib/news/views/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:priobike/common/layout/general_nav.dart';
+import 'package:priobike/common/fx.dart';
+import 'package:priobike/common/layout/spacing.dart';
+import 'package:priobike/common/layout/text.dart';
+import 'package:priobike/common/layout/buttons.dart';
 import 'package:priobike/news/services/news.dart';
 import 'package:priobike/news/views/article_list_item.dart';
 import 'package:provider/provider.dart';
@@ -32,52 +35,57 @@ class NewsViewState extends State<NewsView> {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
-      // Show status bar in opposite color of the background.
-      value: Theme.of(context).brightness == Brightness.light ? SystemUiOverlayStyle.dark : SystemUiOverlayStyle.light,
+      value: SystemUiOverlayStyle.dark,
       child: Scaffold(
-        body: SafeArea(
-          top: true,
-          child: CustomScrollView(
-            slivers: <Widget>[
-              const GeneralNavBarView(
-                title: "Neuigkeiten",
-              ),
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 8),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24),
-                      child: !news.hasLoaded
-                          ? SizedBox(
-                              height: MediaQuery.of(context).size.height * 0.5,
-                              child: const Center(
-                                child: CircularProgressIndicator(),
+        body: Fade(
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          AppBackButton(onPressed: () => Navigator.pop(context)),
+                          const HSpace(),
+                          SubHeader(text: "Neuigkeiten", context: context),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SmallVSpace(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: !news.hasLoaded
+                        ? SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          )
+                        : news.articles.isEmpty
+                            ? const Center(
+                                child: Text('Keine Neuigkeiten verfügbar.'),
+                              )
+                            : ListView(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                children: <Widget>[
+                                  for (int i = 0; i < news.articles.length; i++)
+                                    ArticleListItem(
+                                        article: news.articles[i],
+                                        category: news.categories[news.articles[i].categoryId],
+                                        wasRead: news.readArticles.contains(news.articles[i]),
+                                        totalNumberOfArticles: news.articles.length,
+                                        articleIndex: i)
+                                ],
                               ),
-                            )
-                          : news.articles.isEmpty
-                              ? const Center(
-                                  child: Text('Keine Neuigkeiten verfügbar.'),
-                                )
-                              : ListView(
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  children: <Widget>[
-                                    for (int i = 0; i < news.articles.length; i++)
-                                      ArticleListItem(
-                                          article: news.articles[i],
-                                          category: news.categories[news.articles[i].categoryId],
-                                          wasRead: news.readArticles.contains(news.articles[i]),
-                                          totalNumberOfArticles: news.articles.length,
-                                          articleIndex: i)
-                                  ],
-                                ),
-                    ),
-                    const SizedBox(height: 128),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 128),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
