1d637bff3f067c3e99db71ef4971b7110cd8088c
adeveloper-wq <dajesch@gmail.com>
2023-02-13 13:41:05 +0100

    Add documentation to Prediction interface

--- lib/ride/interfaces/prediction.dart
+++ lib/ride/interfaces/prediction.dart
@@ -4,7 +4,11 @@ abstract class Prediction {
   /// The current prediction quality in [0.0, 1.0]. Calculated periodically.
   double? get predictionQuality;
 
+  /// A toJson method to convert the prediction to a json object. The implementation depends on the specific properties
+  /// of the prediction type.
   Map<String, dynamic> toJson();
 
+  /// A method to calculate the recommendation properties. The implementation depends on the specific properties
+  /// of the prediction type.
   Future<Recommendation?> calculateRecommendation();
 }
