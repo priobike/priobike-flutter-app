631dab3f4a7dbb830291608d313526036b566b0e
Daniel J <dajesch@gmail.com>
2023-02-13 13:49:44 +0100

    Update lib/ride/services/predictor.dart
    Co-authored-by: Philipp <philipp.matthes@tu-dresden.de>

--- lib/ride/services/predictor.dart
+++ lib/ride/services/predictor.dart
@@ -189,10 +189,8 @@ class Predictor implements PredictionComponent {
   Future<void> stopNavigation() async {
     calcTimer?.cancel();
     calcTimer = null;
-    if (client != null) {
-      client?.disconnect();
-      client = null;
-    }
+    client?.disconnect();
+    client = null;
   }
 
   /// Reset the service.
