cae53f45e333b59a4ee1691c8510f3b0b2a20bfc
PaulPickhardt <s2137275@msx.tu-dresden.de>
2023-02-14 15:50:01 +0100

    Fix padding to high for small routes

--- lib/routing/models/route.dart
+++ lib/routing/models/route.dart
@@ -131,7 +131,7 @@ class Route {
     final bounds = this.bounds;
     // Padding is approximately 111m (Approximately 0.001 degrees).
     // See: https://www.usna.edu/Users/oceano/pguth/md_help/html/approx_equivalents.htm
-    const pad = 0.003;
+    const pad = 0.001;
     final coordinatesSouthwest = bounds.southwest["coordinates"] as List;
     final s = coordinatesSouthwest[1] as double;
     final w = coordinatesSouthwest[0] as double;
