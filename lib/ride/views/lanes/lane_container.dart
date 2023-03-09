c06511f6c6f51605e60ec30e27fb7e73ce236c30
adeveloper-wq <dajesch@gmail.com>
2023-03-09 17:15:51 +0100

    Revert back to 1Hz updates

--- lib/ride/views/lanes/lane_container.dart
+++ lib/ride/views/lanes/lane_container.dart
@@ -17,54 +17,48 @@ class LaneContainerWidget extends StatefulWidget {
 
 class LaneContainerWidgetState extends State<LaneContainerWidget> {
   /// The associated ride service, which is injected by the provider.
-  /*late RideMultiLane ride;*/
+  late RideMultiLane ride;
 
   /// The default gauge color for the speedometer.
   static const defaultGaugeColor = Color.fromARGB(255, 50, 50, 50);
-
-  Timer? gradientUpdateTimer;
 
   List<double> stops = [];
   List<Color> colors = [];
 
   /// Called when a listener callback of a ChangeNotifier is fired. Don't rebuild when the gradient gets updated.
-/*  void update() {
+  void update() {
     updateGradient();
     setState(() {});
-  }*/
+  }
 
   @override
   void initState() {
     super.initState();
 
-    /*ride = getIt<RideMultiLane>();
-    ride.addListener(update);*/
-
-    gradientUpdateTimer = Timer.periodic(const Duration(milliseconds: 40), (_) => updateGradient());
+    ride = getIt<RideMultiLane>();
+    ride.addListener(update);
   }
 
   @override
   void dispose() {
-    gradientUpdateTimer?.cancel();
-    /*ride.removeListener(update);*/
+    ride.removeListener(update);
     super.dispose();
   }
 
   Future<void> updateGradient() async {
-    if (getIt<RideMultiLane>().predictionServiceMultiLane == null) {
+    if (ride.predictionServiceMultiLane == null) {
       colors = [defaultGaugeColor, defaultGaugeColor];
       stops = [0.0, 1.0];
       return;
     }
 
-    if (getIt<RideMultiLane>().predictionServiceMultiLane!.predictions[widget.sgId] == null) {
+    if (ride.predictionServiceMultiLane!.recommendations[widget.sgId] == null) {
       colors = [defaultGaugeColor, defaultGaugeColor];
       stops = [0.0, 1.0];
       return;
     }
 
-    final prediction = getIt<RideMultiLane>().predictionServiceMultiLane!.predictions[widget.sgId];
-    final recommendation = await prediction!.calculateRecommendation();
+    final recommendation = ride.predictionServiceMultiLane!.recommendations[widget.sgId];
 
     if (recommendation == null) {
       colors = [defaultGaugeColor, defaultGaugeColor];
@@ -82,8 +76,6 @@ class LaneContainerWidgetState extends State<LaneContainerWidget> {
       final opacity = quality;
       colors.add(Color.lerp(defaultGaugeColor, phase.color, opacity.toDouble()) ?? defaultGaugeColor);
     }
-
-    colors = colors;
 
     stops = Iterable<double>.generate(
       colors.length,
@@ -104,7 +96,6 @@ class LaneContainerWidgetState extends State<LaneContainerWidget> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        //color: Colors.greenAccent,
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
