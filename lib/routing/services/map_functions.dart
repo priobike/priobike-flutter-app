2b29e864bfadec487dcdbf0a7bb0c9f138e1e6f1
Paul Pickhardt <33689888+PaulPickhardt@users.noreply.github.com>
2023-03-15 17:01:16 +0100

    Add center north to routing view (#197)

--- lib/routing/services/map_functions.dart
+++ lib/routing/services/map_functions.dart
@@ -5,6 +5,9 @@ class MapFunctions with ChangeNotifier {
   /// A bool specifying whether the map should get centered on the user position.
   bool needsCentering = false;
 
+  /// A bool specifying whether the map should get centered north.
+  bool needsCenteringNorth = false;
+
   /// The logger for this service.
   final Logger log = Logger("MapFunctionsService");
 
@@ -15,6 +18,12 @@ class MapFunctions with ChangeNotifier {
   /// Apply centering.
   void setCameraCenterOnUserLocation() {
     needsCentering = true;
+    notifyListeners();
+  }
+
+  /// Apply centering of bearing.
+  void setCameraCenterNorth() {
+    needsCenteringNorth = true;
     notifyListeners();
   }
 }
