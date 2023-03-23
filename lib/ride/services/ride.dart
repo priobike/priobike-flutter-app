c3696e867c85ed3f0afc31638334a31c7d042654
adeveloper-wq <dajesch@gmail.com>
2023-03-23 12:38:19 +0100

    Fix next disconnected crossing on route distance

--- lib/ride/services/ride.dart
+++ lib/ride/services/ride.dart
@@ -193,10 +193,12 @@ class Ride with ChangeNotifier {
       if (route!.crossingsDistancesOnRoute[i] > snap.distanceOnRoute) {
         if (route!.crossings[i].connected) continue;
         // The crossing is not connected, so we can use it.
-        routeDistanceOfDisconnectedCrossing = route!.crossingsDistancesOnRoute[i];
-        break;
+        if (route!.crossingsDistancesOnRoute[i] < routeDistanceOfDisconnectedCrossing) {
+          routeDistanceOfDisconnectedCrossing = route!.crossingsDistancesOnRoute[i];
+        }
       }
     }
+
     // If the next disconnected crossing is closer, don't select the next sg just yet.
     if (routeDistanceOfDisconnectedCrossing < routeDistanceOfNextSg) {
       nextSg = null;
