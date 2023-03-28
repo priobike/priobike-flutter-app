5d3ca81b5503ba86c015df9e1009aa6e3e029e46
adeveloper-wq <dajesch@gmail.com>
2023-03-28 16:55:03 +0200

    Add basis for new buttons in center of speedometer

--- lib/ride/views/trafficlight.dart
+++ lib/ride/views/trafficlight.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:priobike/common/layout/ci.dart';
 import 'package:priobike/main.dart';
-import 'package:priobike/ride/messages/prediction.dart';
 import 'package:priobike/ride/services/ride.dart';
-import 'package:priobike/ride/views/cancel_button.dart';
 
 class RideTrafficLightView extends StatefulWidget {
   const RideTrafficLightView({Key? key}) : super(key: key);
@@ -33,98 +34,153 @@ class RideTrafficLightViewState extends State<RideTrafficLightView> {
 
   @override
   Widget build(BuildContext context) {
-    final alternativeView = Container(
-      width: 148,
-      height: 148,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.05),
-        borderRadius: BorderRadius.circular(100),
-      ),
-      child: const CancelButton(
-        borderRadius: 128,
-        text: "Fertig",
-      ),
-    );
-
-    // Don't show a countdown if...
-    if (ride.calcCurrentSG == null) return alternativeView;
-
-    // Check if we have all auxiliary data that the app calculated.
-    if (ride.predictionComponent?.recommendation == null) {
-      return alternativeView;
-    }
-    final recommendation = ride.predictionComponent!.recommendation!;
-    // If the phase change time is null, we hide the countdown.
-    if (recommendation.calcCurrentPhaseChangeTime == null) return alternativeView;
-    // Calculate the countdown.
-    final countdown = recommendation.calcCurrentPhaseChangeTime!.difference(DateTime.now()).inSeconds;
-    // If the countdown is 0 (or negative), we hide the countdown. In this way the user
-    // is not confused if the countdown is at 0 for a few seconds.
-    var countdownLabel = countdown > 0 ? "$countdown" : "";
-    // Show no countdown label for amber and redamber.
-    if (recommendation.calcCurrentSignalPhase == Phase.amber) countdownLabel = "";
-    if (recommendation.calcCurrentSignalPhase == Phase.redAmber) countdownLabel = "";
-
-    final currentPhase = recommendation.calcCurrentSignalPhase;
-
-    final trafficLight = Container(
-      width: 148,
-      height: 148,
-      decoration: BoxDecoration(
-        gradient: RadialGradient(
-          stops: const [0.2, 0.8, 1],
-          colors: [
-            currentPhase.color,
-            currentPhase.color.withOpacity(0.2),
-            currentPhase.color.withOpacity(0),
-          ],
-        ),
-        borderRadius: BorderRadius.circular(64),
-      ),
-      child: Center(
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Transform.translate(
-              offset: const Offset(0, -24),
-              child: Text(
-                countdownLabel,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 64,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 32,
-                      offset: Offset(0, 0),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    ),
-                  ],
-                  fontWeight: FontWeight.w500,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
+    const radius = 120.0;
+    return Stack(
+      children: [
+        // RIGHT BUTTON
+        Transform.translate(
+          offset: const Offset(radius * 0.75, 0),
+          child: Transform.rotate(
+            angle: -pi / 4,
+            child: CenterButton(
+              radius: radius,
+              gradient: const LinearGradient(
+                colors: [CI.blue, Colors.white],
+                stops: [0.4, 1.0],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
               ),
+              onPressed: () => print("test"),
             ),
-            Transform.translate(
-              offset: const Offset(0, 24),
-              child: const CancelButton(),
-            ),
-          ],
+          ),
         ),
-      ),
+        // BOTTOM BUTTON
+        Transform.translate(
+          offset: const Offset(0, radius * 0.75),
+          child: Transform.rotate(
+            angle: pi / 4,
+            child: CenterButton(
+              radius: radius,
+              gradient: const LinearGradient(
+                colors: [CI.blue, Colors.white],
+                stops: [0.4, 1.0],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
+              onPressed: () => print("test"),
+            ),
+          ),
+        ),
+        // LEFT BUTTON
+        Transform.translate(
+          offset: const Offset(-radius * 0.75, 0),
+          child: Transform.rotate(
+            angle: 3 * (pi / 4),
+            child: CenterButton(
+              radius: radius,
+              gradient: const LinearGradient(
+                colors: [CI.blue, Colors.white],
+                stops: [0.4, 1.0],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+              ),
+              onPressed: () => print("test"),
+            ),
+          ),
+        ),
+        // TOP BUTTON
+        Transform.translate(
+          offset: const Offset(0, -radius * 0.75),
+          child: Transform.rotate(
+            angle: -3 * (pi / 4),
+            child: CenterButton(
+              radius: radius,
+              gradient: const LinearGradient(
+                colors: [CI.blue, Colors.white],
+                stops: [0.4, 1.0],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              onPressed: () => print("test"),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CenterButton extends StatelessWidget {
+  final double radius;
+  final LinearGradient gradient;
+  final Function onPressed;
+  const CenterButton({Key? key, required this.radius, required this.onPressed, required this.gradient})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => onPressed,
+        child: CustomPaint(
+          size: Size(radius, radius),
+          painter: CenterButtonPainter(
+            gradient: gradient,
+          ),
+        ));
+  }
+}
+
+class CenterButtonPainter extends CustomPainter {
+  final LinearGradient gradient;
+
+  CenterButtonPainter({required this.gradient});
+
+  void drawButtonShadow(Canvas canvas, Size size, Path path) {
+    final paint = Paint()
+      ..color = CI.blue
+      ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 1);
+    canvas.drawPath(path, paint);
+  }
+
+  void drawButton(Canvas canvas, Size size, Path path) {
+    final paint = Paint()..shader = gradient.createShader(Rect.fromCircle(center: Offset.zero, radius: size.width));
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final path = Path();
+    final holeRadius = size.width / 2;
+    final outerRadius = size.width;
+
+    final borderRadius = 5.0;
+
+    path.lineTo(0, holeRadius - borderRadius);
+    path.arcToPoint(
+      Offset(borderRadius, holeRadius),
+      radius: Radius.circular(borderRadius),
+      clockwise: true,
+    );
+    path.arcToPoint(
+      Offset(holeRadius - borderRadius, 0),
+      radius: Radius.circular(holeRadius),
+      clockwise: false,
     );
 
-    var showCountdown = (ride.calcDistanceToNextSG ?? double.infinity) < 500;
-    showCountdown =
-        showCountdown && (ride.predictionComponent?.prediction?.predictionQuality ?? 0) > Ride.qualityThreshold;
+    path.lineTo(holeRadius, 0);
+    path.lineTo(size.width, 0);
 
-    return AnimatedCrossFade(
-      firstCurve: Curves.easeInOutCubic,
-      secondCurve: Curves.easeInOutCubic,
-      sizeCurve: Curves.easeInOutCubic,
-      duration: const Duration(milliseconds: 500),
-      firstChild: trafficLight,
-      secondChild: alternativeView,
-      crossFadeState: showCountdown ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+    path.arcToPoint(
+      Offset(0, size.height),
+      radius: Radius.circular(outerRadius),
+      clockwise: true,
     );
+
+    drawButtonShadow(canvas, size, path);
+    drawButton(canvas, size, path);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
