71feb4077c1a50aed1bcd4314ade7137a20fcc3d
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-05-05 13:24:24 +0200

    fix searchHistory when tapping on map

--- lib/routing/views/search.dart
+++ lib/routing/views/search.dart
@@ -267,7 +267,6 @@ class RouteSearchState extends State<RouteSearch> {
   /// A callback that is fired when a waypoint is tapped.
   void onWaypointTapped(Waypoint waypoint) {
     geosearch.addToSearchHistory(waypoint);
-    geosearch.saveSearchHistory();
     geosearch.clearGeosearch();
     Navigator.of(context).pop(waypoint);
   }
