0460e041dcc9099e3ac8443a78fac2cfeb1e3d53
Charles Krüger <89537082+kruegercharles@users.noreply.github.com>
2023-05-31 15:04:32 +0200

    Add license info for ray casting algo
    Co-authored-by: Philipp <philipp.matthes@tu-dresden.de>

--- lib/routing/services/ray_casting_algo.dart
+++ lib/routing/services/ray_casting_algo.dart
@@ -1,6 +1,28 @@
 /// This file was taken from: https://github.com/molteo-engineering-team/point_in_polygon
-/// It is used to check if the coordiantes are inside the bounding box of the city
+/// It is used to check if the coordinates are inside the bounding box of the city
 /// by checking if a point is inside a polygon.
+/// 
+/// MIT License
+/// 
+/// Copyright (c) 2021 Molteo
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
 
 class Point {
   Point({required this.x, required this.y});
