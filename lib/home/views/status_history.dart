3131f46fad8745ca30fffcce4becf3703af10513
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-06-07 14:34:58 +0200

    implement basic structure for status history

---
+++ lib/home/views/status_history.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:priobike/home/services/status_history.dart';
+import 'package:priobike/main.dart';
+
+class StatusHistoryView extends StatefulWidget {
+  const StatusHistoryView({Key? key}) : super(key: key);
+
+  @override
+  State<StatusHistoryView> createState() => StatusHistoryViewState();
+}
+
+class StatusHistoryViewState extends State<StatusHistoryView> {
+  /// The associated status history service, which is injected by the provider.
+  late StatusHistory statusHistory;
+
+  /// The maximum value of the status history.
+  double maximumValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    statusHistory = getIt<StatusHistory>();
+    statusHistory.addListener(update);
+    statusHistory.fetch();
+  }
+
+  /// Called when a listener callback of a ChangeNotifier is fired.
+  void update() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    statusHistory.removeListener(update);
+    super.dispose();
+  }
+
+  /// Calculates the maximum value of the status history.
+  void calculateMaximum() {
+    if (!statusHistory.hasLoaded) return;
+
+    // TODO: Calculate the maximum value of the status history to set the diagram scale.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: statusHistory.hasLoaded
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Container(
+                height: 100,
+                color: Colors.red,
+                child: const Center(
+                  child: Text("Status History"),
+                ),
+              ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
