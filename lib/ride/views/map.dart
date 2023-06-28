8386dd62b2883f0535a5f5a17f2df4ccc4fb3d3d
adeveloper-wq <dajesch@gmail.com>
2023-06-28 08:19:53 +0200

    Don't stick camera to selected traffic light

--- lib/ride/views/map.dart
+++ lib/ride/views/map.dart
@@ -153,18 +153,6 @@ class RideMapViewState extends State<RideMapView> {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     if (!mounted) return;
     await TrafficLightLayer(isDark).update(mapController!);
-
-    if (ride.userSelectedSG != null) {
-      // The camera target is the selected SG.
-      final cameraTarget = LatLng(ride.userSelectedSG!.position.lat, ride.userSelectedSG!.position.lon);
-      await mapController?.flyTo(
-        mapbox.CameraOptions(
-          center: mapbox.Point(coordinates: mapbox.Position(cameraTarget.longitude, cameraTarget.latitude)).toJson(),
-          padding: mapbox.MbxEdgeInsets(bottom: 200, top: 0, left: 0, right: 0),
-        ),
-        mapbox.MapAnimationOptions(duration: 200),
-      );
-    }
   }
 
   /// Snap the location indicator to the start of the route.
@@ -432,6 +420,17 @@ class RideMapViewState extends State<RideMapView> {
       final sgIdx = int.tryParse(id.split("-")[2]);
       if (sgIdx == null) return;
       ride.userSelectSG(sgIdx);
+      if (ride.userSelectedSG != null) {
+        // The camera target is the selected SG.
+        final cameraTarget = LatLng(ride.userSelectedSG!.position.lat, ride.userSelectedSG!.position.lon);
+        await mapController?.flyTo(
+          mapbox.CameraOptions(
+            center: mapbox.Point(coordinates: mapbox.Position(cameraTarget.longitude, cameraTarget.latitude)).toJson(),
+            padding: mapbox.MbxEdgeInsets(bottom: 200, top: 0, left: 0, right: 0),
+          ),
+          mapbox.MapAnimationOptions(duration: 200),
+        );
+      }
     }
   }
 
