4b6fd0bbf3a859ca3e86a2b7b60d20d8dc719b76
Paul Pickhardt <33689888+PaulPickhardt@users.noreply.github.com>
2023-06-29 10:02:20 +0200

    Statuscard new pageview (#266)
    * Change pageview to custom pageview with height
    
    * Make pageview keep childs loaded and prevent custompaint from repainting
    
    * Remove async
    
    * Update lib/status/views/status_tabs.dart
    
    Co-authored-by: Daniel J. <dajesch@gmail.com>
    
    * Add comment and commit suggestion
    
    ---------
    
    Co-authored-by: Daniel J. <dajesch@gmail.com>

--- lib/status/views/status.dart
+++ lib/status/views/status.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:priobike/common/layout/ci.dart';
+import 'package:priobike/common/layout/spacing.dart';
 import 'package:priobike/common/layout/text.dart';
 import 'package:priobike/common/layout/tiles.dart';
 import 'package:priobike/main.dart';
@@ -85,71 +86,79 @@ class StatusViewState extends State<StatusView> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 2),
+      padding: const EdgeInsets.symmetric(horizontal: 6),
       child: Tile(
         fill: isProblem ? CI.red : Theme.of(context).colorScheme.background,
         shadowIntensity: isProblem ? 0.2 : 0.05,
         shadow: isProblem ? CI.red : Colors.black,
         onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SGStatusMapView())),
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  isProblem
-                      ? BoldContent(text: "Vorübergehende Störung", context: context, color: Colors.white)
-                      : BoldContent(text: "Datenverfügbarkeit", context: context),
-                  const SizedBox(height: 2),
-                  isProblem
-                      ? Small(
-                          text: text ?? "Lade Daten...",
-                          context: context,
-                          color: Colors.white,
-                        )
-                      : Small(
-                          text: text ?? "Lade Daten...",
-                          context: context,
+            BoldSmall(text: "Jetzt", context: context),
+            const SmallVSpace(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      isProblem
+                          ? BoldContent(text: "Vorübergehende Störung", context: context, color: Colors.white)
+                          : BoldContent(text: "Datenverfügbarkeit", context: context),
+                      const SizedBox(height: 6),
+                      isProblem
+                          ? Small(
+                              text: text ?? "Lade Daten...",
+                              context: context,
+                              color: Colors.white,
+                            )
+                          : Small(
+                              text: text ?? "Lade Daten...",
+                              context: context,
+                            ),
+                    ],
+                  ),
+                ),
+                // Show a progress indicator with the pct value.
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                        width: 42,
+                        height: 42,
+                        child: CircularProgressIndicator(
+                          value: predictionStatusSummary.isLoading ? null : goodPct,
+                          strokeWidth: 6,
+                          backgroundColor:
+                              isProblem ? const Color.fromARGB(255, 120, 0, 50) : CI.green.withOpacity(0.2),
+                          valueColor: isProblem
+                              ? const AlwaysStoppedAnimation<Color>(Colors.white)
+                              : const AlwaysStoppedAnimation<Color>(CI.green),
                         ),
-                ],
-              ),
-            ),
-            // Show a progress indicator with the pct value.
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  SizedBox(
-                    width: 42,
-                    height: 42,
-                    child: CircularProgressIndicator(
-                      value: predictionStatusSummary.isLoading ? null : goodPct,
-                      strokeWidth: 6,
-                      backgroundColor: isProblem ? const Color.fromARGB(255, 120, 0, 50) : CI.green.withOpacity(0.2),
-                      valueColor: isProblem
-                          ? const AlwaysStoppedAnimation<Color>(Colors.white)
-                          : const AlwaysStoppedAnimation<Color>(CI.green),
-                    ),
+                      ),
+                      Opacity(
+                        opacity: 0.2,
+                        child: Icon(
+                          Icons.chevron_right_rounded,
+                          color: isProblem ? Colors.white : CI.green,
+                          size: 42,
+                        ),
+                      ),
+                      BoldSmall(
+                        text: "${((goodPct ?? 0) * 100).round()}%",
+                        context: context,
+                        color: isProblem ? Colors.white : Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ],
                   ),
-                  Opacity(
-                    opacity: 0.2,
-                    child: Icon(
-                      Icons.chevron_right_rounded,
-                      color: isProblem ? Colors.white : CI.green,
-                      size: 42,
-                    ),
-                  ),
-                  BoldSmall(
-                    text: "${((goodPct ?? 0) * 100).round()}%",
-                    context: context,
-                    color: isProblem ? Colors.white : Theme.of(context).colorScheme.onBackground,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
