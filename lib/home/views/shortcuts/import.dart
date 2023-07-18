4c1a0159ac6da9b0565aabeca5ed9fa524e6b3e4
PaulPickhardt <s2137275@msx.tu-dresden.de>
2023-07-18 15:38:47 +0200

    Refactor shortcut sheet

--- lib/home/views/shortcuts/import.dart
+++ lib/home/views/shortcuts/import.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart' hide Shortcuts;
 import 'package:flutter/services.dart';
-import 'package:get_it/get_it.dart';
 import 'package:priobike/common/layout/text.dart';
 import 'package:priobike/common/layout/tiles.dart';
 import 'package:priobike/home/models/shortcut_route.dart';
-import 'package:priobike/home/services/shortcuts.dart';
+import 'package:priobike/home/views/save_shortcut_dialog.dart';
 import 'package:priobike/home/views/shortcuts/qr_code.dart';
 import 'package:priobike/logging/toast.dart';
 import 'package:priobike/routing/models/waypoint.dart';
@@ -40,66 +39,13 @@ class ImportShortcutDialogState<E> extends State<ImportShortcutDialog<E>> {
       for (var i = 0; i < coordinates.length; i++) {
         waypoints.add(Waypoint(coordinates[i]![0], coordinates[i]![1], address: "Wegpunkt ${i + 1}"));
       }
-      showSaveShortcutSheet(ShortcutRoute(name: "Strecke von Google Maps", waypoints: waypoints));
+      if (mounted) {
+        showSaveShortcutSheet(context, ShortcutRoute(name: "Strecke von Google Maps", waypoints: waypoints));
+      }
       return;
     }
 
     ToastMessage.showError("Kein valider Routen-Link: ${data.text!.substring(0, 20)}...");
-  }
-
-  /// Show a sheet to save the current route as a shortcut.
-  void showSaveShortcutSheet(ShortcutRoute shortcut) {
-    final shortcuts = GetIt.instance.get<Shortcuts>();
-    showDialog(
-      context: context,
-      builder: (_) {
-        final nameController = TextEditingController();
-        return AlertDialog(
-          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 24),
-          insetPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40.0),
-          title: BoldContent(
-            text: 'Bitte gib einen Namen an, unter dem die Strecke gespeichert werden soll.',
-            context: context,
-          ),
-          content: SizedBox(
-            height: 78,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: nameController,
-                  maxLength: 20,
-                  decoration: const InputDecoration(hintText: 'Heimweg, Zur Arbeit, ...'),
-                ),
-              ],
-            ),
-          ),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(24)),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                final name = nameController.text;
-                if (name.trim().isEmpty) {
-                  ToastMessage.showError("Name darf nicht leer sein.");
-                  return;
-                }
-                shortcut.name = name;
-                await shortcuts.saveNewShortcutObject(shortcut);
-                ToastMessage.showSuccess("Route gespeichert!");
-                // ignore: use_build_context_synchronously
-                Navigator.pop(context);
-              },
-              child: BoldContent(
-                text: 'Speichern',
-                color: Theme.of(context).colorScheme.primary,
-                context: context,
-              ),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
