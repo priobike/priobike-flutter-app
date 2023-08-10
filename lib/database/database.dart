7c159ffe634c0c128ecbbeffde67363bfd0941f0
Mattis Lahr <Lahrma@t-online.de>
2023-08-10 16:37:53 +0200

    Changed the database package, since listening to changes was not supported before

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,32 +1,29 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'package:sqflite/sqflite.dart';
+
+part 'database.g.dart';
 
 /// Class holding the database required for the gamification data structure. It can be accessed as a Singleton.
-class AppDatabase {
-  static const String dbFileName = "main.db";
-
+@DriftDatabase(tables: [])
+class AppDatabase extends _$AppDatabase {
   /// Static instance of the class to access it as a singleton.
   static final AppDatabase instance = AppDatabase();
 
-  /// Instance of the database held by the class.
-  static Database? _database;
+  AppDatabase() : super(_openConnection());
 
-  /// Returns the database. If the database is null, initialize it beforehand.
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB();
-    return _database!;
-  }
+  @override
+  int get schemaVersion => 1;
+}
 
-  /// Initialize, open and return the database.
-  Future<Database> _initDB() async {
-    final dbPath = await getDatabasesPath();
-    final path = p.join(dbPath, dbFileName);
-    return openDatabase(path, version: 5, onCreate: _createDB);
-  }
-
-  /// Callback function for when the database is created. Creates the required database tables.
-  Future _createDB(Database db, int version) async {
-    //TODO: intialize required tables
-  }
+/// Create database file at appropriate location.
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
 }
--- lib/database/repository.dart
+++ lib/database/repository.dart
@@ -1,34 +1,47 @@
+import 'package:drift/drift.dart';
 import 'package:priobike/database/database.dart';
 import 'package:priobike/database/repository_object.dart';
-import 'package:sqflite/sqflite.dart';
 
 /// Abstract repository class to be extendet by the actual database repositories. Each
 /// repository manages database transactions of a specific kind of [RepositoryObject].
-abstract class Repository<T extends RepositoryObject> {
-  late AppDatabase _instance;
+abstract class Repository<T extends Insertable<dynamic>> {
+  late AppDatabase _db;
 
   /// Name of the database table corresponding to this repository.
-  final String _tableName;
+  final TableInfo _table;
 
-  Repository(this._tableName) {
-    _instance = AppDatabase.instance;
+  Repository(this._table) {
+    _db = AppDatabase.instance;
   }
 
-  /// Insert object into the database.
-  Future<T> create(T object) async {
-    final db = await _instance.database;
-    final id = await db.insert(_tableName, object.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
-    return object.copyWithId(id: id);
+  /// Insert object into the database. Returns null if the object wasn't inserted or the object with the new id.
+  Future<T?> create(UpdateCompanion<T> object) async {
+    try {
+      final id = await _db.into(_table).insert(object);
+      return getById(id);
+    } on Exception catch (_) {
+      return null;
+    }
   }
 
-  /// Update object which is already in the database.
-  Future<void> update(T object) async {
-    final db = await _instance.database;
-    await db.update(
-      _tableName,
-      object.toMap(),
-      where: 'id = ?',
-      whereArgs: [object.id],
-    );
+  /// Update object which is already in the database. Returns true if the change was successfully made.
+  Future<bool> update(T object) async {
+    return _db.update(_table).replace(object);
+  }
+
+  /// Delete object from the database by id. Returns true if an object was deleted successfully.
+  Future<bool> delete(T object) async {
+    var result = await _db.delete(_table).delete(object);
+    return result > 0;
+  }
+
+  /// Get object from repository table by id.
+  Future<T?> getById(id) {
+    throw UnimplementedError();
+  }
+
+  /// Get all objects from the repositories table.
+  Future<List<T>> getAll(String orderBy) async {
+    return _db.select(_table).get() as Future<List<T>>;
   }
 }
