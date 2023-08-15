25445e18dad8c0e65428ee6795494f4a8f36553a
Paul Pickhardt <33689888+PaulPickhardt@users.noreply.github.com>
2023-08-15 13:21:21 +0200

    Fix text not aligned correctly (#287)

--- lib/home/views/main.dart
+++ lib/home/views/main.dart
@@ -266,8 +266,8 @@ class HomeViewState extends State<HomeView> with WidgetsBindingObserver, RouteAw
                       ),
                     ),
                   const VSpace(),
-                  const BlendIn(
-                    child: StatusTabsView(),
+                  BlendIn(
+                    child: StatusTabsView(triggerRebuild: () => setState(() {})),
                   ),
                   const VSpace(),
                   BlendIn(
