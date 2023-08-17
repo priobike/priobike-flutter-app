5946eb93020c8d1b6109e8a396f903e5f6d87456
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-08-17 11:09:40 +0200

    use tile already in use instead of new one

--- lib/tracking/views/track_history_item.dart
+++ lib/tracking/views/track_history_item.dart
@@ -84,157 +84,155 @@ class TrackHistoryItemViewState extends State<TrackHistoryItemView> {
         ? '${(secondsDriven ~/ 60).toString().padLeft(2, '0')}:${(secondsDriven % 60).toString().padLeft(2, '0')}\nMinuten'
         : null;
 
-    return TextButton(
-      onPressed: () => showAppSheet(
-        context: context,
-        builder: (context) => TrackDetailsDialog(
-            track: widget.track, startImage: widget.startImage, destinationImage: widget.destinationImage),
-      ),
-      child: Container(
-        alignment: Alignment.centerLeft,
-        width: widget.width,
-        padding: EdgeInsets.only(right: widget.rightPad, bottom: 24),
-        child: Tile(
-          shadow: const Color.fromARGB(255, 0, 0, 0),
-          shadowIntensity: 0.08,
-          padding: const EdgeInsets.all(4),
-          fill: Theme.of(context).colorScheme.background,
-          splash: Theme.of(context).colorScheme.primary,
-          content: SizedBox(
-            height: 160,
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Positioned.fill(
-                  child: Container(
-                    foregroundDecoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: Theme.of(context).colorScheme.brightness == Brightness.dark
-                            ? [
-                                Theme.of(context).colorScheme.background,
-                                Theme.of(context).colorScheme.background,
-                                Theme.of(context).colorScheme.background.withOpacity(0.9),
-                                Theme.of(context).colorScheme.background.withOpacity(0.8),
-                                Theme.of(context).colorScheme.background.withOpacity(0.7),
-                              ]
-                            : [
-                                Theme.of(context).colorScheme.background,
-                                Theme.of(context).colorScheme.background,
-                                Theme.of(context).colorScheme.background.withOpacity(0.6),
-                                Theme.of(context).colorScheme.background.withOpacity(0.5),
-                                Theme.of(context).colorScheme.background.withOpacity(0.3),
-                              ],
-                      ),
-                      borderRadius: BorderRadius.circular(20.0),
+    return Container(
+      alignment: Alignment.centerLeft,
+      width: widget.width,
+      padding: EdgeInsets.only(right: widget.rightPad, bottom: 24),
+      child: Tile(
+        onPressed: () => showAppSheet(
+          context: context,
+          builder: (context) => TrackDetailsDialog(
+              track: widget.track, startImage: widget.startImage, destinationImage: widget.destinationImage),
+        ),
+        shadow: const Color.fromARGB(255, 0, 0, 0),
+        shadowIntensity: 0.08,
+        padding: const EdgeInsets.all(4),
+        fill: Theme.of(context).colorScheme.background,
+        splash: Theme.of(context).colorScheme.primary,
+        content: SizedBox(
+          height: 160,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Positioned.fill(
+                child: Container(
+                  foregroundDecoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: Theme.of(context).colorScheme.brightness == Brightness.dark
+                          ? [
+                              Theme.of(context).colorScheme.background,
+                              Theme.of(context).colorScheme.background,
+                              Theme.of(context).colorScheme.background.withOpacity(0.9),
+                              Theme.of(context).colorScheme.background.withOpacity(0.8),
+                              Theme.of(context).colorScheme.background.withOpacity(0.7),
+                            ]
+                          : [
+                              Theme.of(context).colorScheme.background,
+                              Theme.of(context).colorScheme.background,
+                              Theme.of(context).colorScheme.background.withOpacity(0.6),
+                              Theme.of(context).colorScheme.background.withOpacity(0.5),
+                              Theme.of(context).colorScheme.background.withOpacity(0.3),
+                            ],
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image(
-                        image: Theme.of(context).colorScheme.brightness == Brightness.dark
-                            ? const AssetImage('assets/images/map-dark.png')
-                            : const AssetImage('assets/images/map-light.png'),
-                        fit: BoxFit.cover,
-                      ),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image(
+                      image: Theme.of(context).colorScheme.brightness == Brightness.dark
+                          ? const AssetImage('assets/images/map-dark.png')
+                          : const AssetImage('assets/images/map-light.png'),
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                Positioned(
-                  top: 10,
-                  left: 10,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "$day.",
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                          height: 0.9,
-                          foreground: Paint()
-                            ..shader = const LinearGradient(
-                              colors: [
-                                CI.blue,
-                                CI.blueLight,
-                              ],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                            ).createShader(const Rect.fromLTWH(0.0, 0.0, 90.0, 90.0)),
-                        ),
-                      ),
-                      const SmallHSpace(),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "$monthName\n${year.toString()}",
-                            style: const TextStyle(
-                              fontSize: 11,
-                              height: 1.2,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                if (trackDurationFormatted != null)
-                  Positioned(
-                    bottom: 10,
-                    left: 10,
-                    child: Text(
-                      trackDurationFormatted,
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "$day.",
                       style: TextStyle(
-                        fontSize: 11,
-                        height: 1.2,
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.6),
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        height: 0.9,
+                        foreground: Paint()
+                          ..shader = const LinearGradient(
+                            colors: [
+                              CI.blue,
+                              CI.blueLight,
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ).createShader(const Rect.fromLTWH(0.0, 0.0, 90.0, 90.0)),
                       ),
                     ),
-                  ),
-                const Positioned(
+                    const SmallHSpace(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "$monthName\n${year.toString()}",
+                          style: const TextStyle(
+                            fontSize: 11,
+                            height: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              if (trackDurationFormatted != null)
+                Positioned(
                   bottom: 10,
-                  right: 10,
+                  left: 10,
                   child: Text(
-                    "Route",
+                    trackDurationFormatted,
                     style: TextStyle(
                       fontSize: 11,
                       height: 1.2,
-                      color: CI.blue,
-                    ),
-                  ),
-                ),
-                if (routeNodes.isNotEmpty)
-                  Positioned(
-                    bottom: 10,
-                    right: 20,
-                    child: SizedBox(
-                      height: widget.width * 0.3,
-                      width: widget.width * 0.3,
-                      child: RoutePictogram(
-                        route: routeNodes,
-                        startImage: widget.startImage,
-                        destinationImage: widget.destinationImage,
-                      ),
-                    ),
-                  ),
-                Positioned(
-                  right: 0,
-                  top: 0,
-                  child: IconButton(
-                    key: const ValueKey("delete"),
-                    onPressed: () => getIt<Tracking>().deleteTrack(widget.track),
-                    icon: Icon(
-                      Icons.delete_rounded,
-                      size: 22,
                       color: Theme.of(context).colorScheme.onBackground.withOpacity(0.6),
                     ),
                   ),
                 ),
-              ],
-            ),
+              const Positioned(
+                bottom: 10,
+                right: 10,
+                child: Text(
+                  "Route",
+                  style: TextStyle(
+                    fontSize: 11,
+                    height: 1.2,
+                    color: CI.blue,
+                  ),
+                ),
+              ),
+              if (routeNodes.isNotEmpty)
+                Positioned(
+                  bottom: 10,
+                  right: 20,
+                  child: SizedBox(
+                    height: widget.width * 0.3,
+                    width: widget.width * 0.3,
+                    child: RoutePictogram(
+                      route: routeNodes,
+                      startImage: widget.startImage,
+                      destinationImage: widget.destinationImage,
+                    ),
+                  ),
+                ),
+              Positioned(
+                right: 0,
+                top: 0,
+                child: IconButton(
+                  key: const ValueKey("delete"),
+                  onPressed: () => getIt<Tracking>().deleteTrack(widget.track),
+                  icon: Icon(
+                    Icons.delete_rounded,
+                    size: 22,
+                    color: Theme.of(context).colorScheme.onBackground.withOpacity(0.6),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
