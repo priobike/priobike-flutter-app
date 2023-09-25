541e1007e7efca3bd0cd8d7e7045a0d627d0ff29
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-09-25 10:16:31 +0200

    put early return on top of function

--- lib/tracking/views/track_history.dart
+++ lib/tracking/views/track_history.dart
@@ -105,10 +105,11 @@ class TrackHistoryViewState extends State<TrackHistoryView> {
 
   @override
   Widget build(BuildContext context) {
-    const double shortcutRightPad = 12;
-    final shortcutWidth = ((MediaQuery.of(context).size.width - 36) / 2) - shortcutRightPad;
     if (startImage == null || destinationImage == null) return Container();
     if (newestTracks.isEmpty) return Container();
+
+    const double shortcutRightPad = 12;
+    final shortcutWidth = ((MediaQuery.of(context).size.width - 36) / 2) - shortcutRightPad;
 
     List<Widget> views = [
       AnimatedContainer(
