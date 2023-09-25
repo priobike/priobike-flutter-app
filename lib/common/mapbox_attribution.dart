5e0f7d62edd954fa429dc2ab6dae8dd79636650a
florianpix <florian.pix.97@outlook.com>
2023-09-25 11:13:18 +0200

    formatting;

--- lib/common/mapbox_attribution.dart
+++ lib/common/mapbox_attribution.dart
@@ -32,10 +32,13 @@ class MapboxAttribution extends StatelessWidget {
   }) : super(key: key);
 
   /// Used to show the attribution dialog.
-  /// (Only if the battery saving mode is used because otherwise the Mapbox native dialog is used.)
-  /// (In the battery saving mode the Mapbox native dialog can't be used because it is outside of the visible display area.)
+  /// (Only if the battery saving mode is used
+  /// because otherwise the Mapbox native dialog is used.)
+  /// (In the battery saving mode the Mapbox native dialog can't be used
+  /// because it is outside of the visible display area.)
   static showAttribution(BuildContext context) {
-    final bool satelliteAttributionRequired = getIt<MapDesigns>().mapDesign.name == 'Satellit';
+    final bool satelliteAttributionRequired = getIt<MapDesigns>()
+        .mapDesign.name == 'Satellit';
     final List<Map<String, dynamic>> attributionEntries = [
       {
         'title': 'Mapbox',
@@ -58,33 +61,39 @@ class MapboxAttribution extends StatelessWidget {
     const title = "Powered by Mapbox Maps";
 
     showDialog<String>(
-      context: context,
-      builder: (BuildContext context) {
-        return DialogLayout(
-          title: title,
-          text: "",
-          icon: Icons.map,
-          iconColor: Theme.of(context).colorScheme.primary,
-          actions: [
-            for (final entry in attributionEntries)
+        context: context,
+        builder: (BuildContext context) {
+          return DialogLayout(
+            title: title,
+            text: "",
+            icon: Icons.map,
+            iconColor: Theme.of(context).colorScheme.primary,
+            actions: [
+              for (final entry in attributionEntries)
+                BigButton(
+                  label: entry['title']!,
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    await launchUrl(
+                        entry['url']!,
+                        mode: LaunchMode.externalApplication
+                    );
+                  },
+                  boxConstraints: BoxConstraints(
+                      minWidth: MediaQuery.of(context).size.width
+                  ),
+                ),
               BigButton(
-                label: entry['title']!,
-                onPressed: () async {
-                  Navigator.pop(context);
-                  await launchUrl(entry['url']!, mode: LaunchMode.externalApplication);
-                },
-                boxConstraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+                label: "Abbrechen",
+                fillColor: CI.blueDark,
+                onPressed: () => Navigator.pop(context),
+                boxConstraints: BoxConstraints
+                  (minWidth: MediaQuery.of(context).size.width
+                ),
               ),
-            BigButton(
-              label: "Abbrechen",
-              fillColor: CI.blueDark,
-              onPressed: () => Navigator.pop(context),
-              boxConstraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
-            ),
-          ],
-        );
-      }
-    );
+            ],
+          );
+        });
   }
 
   @override
