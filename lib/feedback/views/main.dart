37e96ae0216f2445df5af90dd60bd97da599f1cf
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-10-05 13:15:35 +0200

    set appbar hight feedback view to 0 and restore old design

--- lib/feedback/views/main.dart
+++ lib/feedback/views/main.dart
@@ -147,41 +147,6 @@ class FeedbackViewState extends State<FeedbackView> {
     );
   }
 
-  Widget _renderTitle(String start, String end) {
-    if (start == "" || end == "") {
-      return BoldContent(
-        text: "Fahrt",
-        context: context,
-        textAlign: TextAlign.center,
-      );
-    } else {
-      return Wrap(
-        alignment: WrapAlignment.center,
-        runAlignment: WrapAlignment.center,
-        direction: Axis.horizontal,
-        runSpacing: 8,
-        children: [
-          Content(
-            text: "Von ",
-            context: context,
-          ),
-          Content(
-            text: start,
-            context: context,
-          ),
-          Content(
-            text: " nach ",
-            context: context,
-          ),
-          Content(
-            text: end,
-            context: context,
-          )
-        ],
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     if (feedback.isSendingFeedback) return renderLoadingIndicator();
@@ -203,30 +168,60 @@ class FeedbackViewState extends State<FeedbackView> {
             statusBarColor: Colors.transparent,
             systemNavigationBarColor: Theme.of(context).colorScheme.primary,
           ),
-          title: _renderTitle(start, end),
-          centerTitle: true,
+          toolbarHeight: 0,
         ),
-        body:
-            // AnnotatedRegion<SystemUiOverlayStyle>(
-            //   value: Theme.of(context).brightness == Brightness.light
-            //       ? SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent)
-            //       : SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent),
-            //   child:
-            SafeArea(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height - bottomSheetHeight,
-            child: SingleChildScrollView(
-              key: const ValueKey("feedback_scroll_view"),
-              child: (startImage != null && destinationImage != null)
-                  ? TrackHistoryItemDetailView(
-                      track: tracking.previousTracks!.last,
-                      startImage: startImage!,
-                      destinationImage: destinationImage!,
-                    )
-                  : Container(),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height - bottomSheetHeight,
+          child: SingleChildScrollView(
+            key: const ValueKey("feedback_scroll_view"),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.fromLTRB(24, MediaQuery.of(context).padding.top + 24, 24, 0),
+                  child: () {
+                    if (start == "" || end == "") {
+                      return BoldContent(
+                        text: "Fahrt",
+                        context: context,
+                        textAlign: TextAlign.center,
+                      );
+                    } else {
+                      return Wrap(
+                        alignment: WrapAlignment.center,
+                        runAlignment: WrapAlignment.center,
+                        direction: Axis.horizontal,
+                        runSpacing: 8,
+                        children: [
+                          Content(
+                            text: "Von ",
+                            context: context,
+                          ),
+                          Content(
+                            text: start,
+                            context: context,
+                          ),
+                          Content(
+                            text: " nach ",
+                            context: context,
+                          ),
+                          Content(
+                            text: end,
+                            context: context,
+                          )
+                        ],
+                      );
+                    }
+                  }(),
+                ),
+                if (startImage != null && destinationImage != null)
+                  TrackHistoryItemDetailView(
+                    track: tracking.previousTracks!.last,
+                    startImage: startImage!,
+                    destinationImage: destinationImage!,
+                  ),
+              ],
             ),
           ),
-          // ),
         ),
         bottomSheet: Container(
           width: MediaQuery.of(context).size.width,
@@ -247,16 +242,13 @@ class FeedbackViewState extends State<FeedbackView> {
                 onPressed: () => submit(),
                 boxConstraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width - 24),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 0),
-                child: BigButton(
-                  iconColor: Colors.white,
-                  icon: Icons.save_rounded,
-                  fillColor: Theme.of(context).colorScheme.background.withOpacity(0.25),
-                  label: "Strecke speichern",
-                  onPressed: () => showSaveShortcutSheet(context),
-                  boxConstraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width - 24),
-                ),
+              BigButton(
+                iconColor: Colors.white,
+                icon: Icons.save_rounded,
+                fillColor: Theme.of(context).colorScheme.background.withOpacity(0.25),
+                label: "Strecke speichern",
+                onPressed: () => showSaveShortcutSheet(context),
+                boxConstraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width - 24),
               ),
             ],
           ),
