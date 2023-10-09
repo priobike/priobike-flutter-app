fa07298fffa05aae36a834bf16364232b2d58620
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-10-09 13:04:14 +0200

    remove unnecessary import

--- lib/ride/views/finish_button.dart
+++ lib/ride/views/finish_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:priobike/common/layout/buttons.dart';
 import 'package:priobike/common/layout/dialog.dart';
 import 'package:priobike/common/layout/spacing.dart';
