30f2a9c42fcb3f9b077c6189137711ed2dc262d5
PaulPickhardt <s2137275@msx.tu-dresden.de>
2023-12-07 14:30:40 +0100

    Fix tertiary buttons

--- lib/common/layout/buttons.dart
+++ lib/common/layout/buttons.dart
@@ -168,7 +168,7 @@ class SmallIconButtonTertiary extends StatelessWidget {
       child: RawMaterialButton(
         elevation: 0,
         // Hide ugly material shadows.
-        fillColor: fill ?? Colors.transparent,
+        fillColor: fill ?? Theme.of(context).colorScheme.background,
         splashColor: splash ?? Theme.of(context).colorScheme.onTertiary,
         onPressed: onPressed,
         shape: withBorder
@@ -478,7 +478,7 @@ class BigButtonTertiary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      fillColor: fillColor ?? Colors.transparent,
+      fillColor: fillColor ?? Theme.of(context).colorScheme.background,
       splashColor: splashColor ?? Theme.of(context).colorScheme.onTertiary,
       constraints: boxConstraints,
       // Hide ugly material shadows.
@@ -758,7 +758,7 @@ class IconTextButtonTertiary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      fillColor: fillColor ?? Colors.transparent,
+      fillColor: fillColor ?? Theme.of(context).colorScheme.background,
       splashColor: splashColor ?? Theme.of(context).colorScheme.onTertiary,
       constraints: boxConstraints,
       // Hide ugly material shadows.
