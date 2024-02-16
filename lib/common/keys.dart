28e1ebfeed0fda831ea9bb0a7112391844f1bdb9
adeveloper-wq <dajesch@gmail.com>
2024-02-16 13:09:01 +0100

    Add shlink release api key

--- lib/common/keys.dart
+++ lib/common/keys.dart
@@ -3,5 +3,5 @@ class Keys {
       "pk.eyJ1Ijoic25ybXR0aHMiLCJhIjoiY2w0ZWVlcWt5MDAwZjNjbW5nMHNvN3kwNiJ9.upoSvMqKIFe3V_zPt1KxmA";
   static const linkShortenerApiKeyStaging = "8a1e47f1-36ac-44e8-b648-aae112f97208";
   static const linkShortenerApiKeyProduction = "EeLErcMKtThp36epbHZbTLJchhMAFgiBGty7Rbov";
-  static const linkShortenerApiKeyRelease = "de2nXtEviGPutZkK3nb3UME4zjPUBDFbYwEcRYY2";
+  static const linkShortenerApiKeyRelease = "8EmdUGwC52dtaVCT0A3w4ccv0GGjEmyd5X7cxr89";
 }
