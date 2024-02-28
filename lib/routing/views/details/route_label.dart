b7547cc923ff3460d03005634508e9e295d245fd
Daniel J <dajesch@gmail.com>
2024-02-28 13:06:18 +0100

    Route labels as flutter widget (Refactor) (PR to Pauls branch) (#416)
    * Some refactoring ideas
    
    * Apply small fix
    
    * Minor stuff
    
    * Refactoring version 2
    
    * Finish refactoring merging Pauls with my code
    
    * Delete unused file
    
    * Add back accidentally deleted code
    
    * Update comments and reorder code
    
    * Typo
    
    * Fix bug in get valid combination function and add minor formal comments
    
    * refacotr
    
    ---------
    
    Co-authored-by: PaulPickhardt <s2137275@msx.tu-dresden.de>

--- lib/routing/views/details/route_label.dart
+++ lib/routing/views/details/route_label.dart
@@ -1,10 +1,42 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:priobike/common/layout/ci.dart';
 import 'package:priobike/common/layout/text.dart';
-import 'package:priobike/routing/models/route_label.dart';
+import 'package:priobike/main.dart';
+import 'package:priobike/routing/services/route_labels.dart';
+import 'package:priobike/routing/services/routing.dart';
 
-class RouteLabelIcon extends StatelessWidget {
-  final RouteLabel routeLabel;
+class RouteLabel extends StatefulWidget {
+  /// The id of the route.
+  final int routId;
+
+  /// The alignment of the route label.
+  final RouteLabelAlignment alignment;
+
+  /// If the map is currently moving.
+  final bool isMapMoving;
+
+  const RouteLabel({
+    super.key,
+    required this.routId,
+    required this.alignment,
+    required this.isMapMoving,
+  });
+
+  @override
+  RouteLabelState createState() => RouteLabelState();
+}
+
+class RouteLabelState extends State<RouteLabel> {
+  /// The selected state of the route label.
+  bool selected = false;
+
+  /// The time text of the route label.
+  String timeText = "";
+
+  /// The secondary text of the route label.
+  String? secondaryText;
 
   /// The corner margin for the icon that is applied to the route label.
   static const double cornerIconMargin = 10;
@@ -18,7 +50,39 @@ class RouteLabelIcon extends StatelessWidget {
   /// The max height used for calculations.
   static const double maxHeight = 60;
 
-  const RouteLabelIcon({super.key, required this.routeLabel});
+  /// The opacity of the route label.
+  double opacity = 0;
+
+  /// The timer for the opacity animation.
+  Timer? opacityTimer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final routing = getIt<Routing>();
+
+    if (routing.selectedRoute != null) selected = routing.selectedRoute!.id == widget.routId;
+    if (routing.allRoutes != null && routing.allRoutes!.length > widget.routId) {
+      final route = routing.allRoutes![widget.routId];
+      timeText = route.timeText;
+    }
+  }
+
+  Future<void> animateOpacity() async {
+    opacityTimer?.cancel();
+    opacityTimer = Timer(const Duration(milliseconds: 150), () {
+      setState(() {
+        opacity = 1;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    opacityTimer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,81 +90,93 @@ class RouteLabelIcon extends StatelessWidget {
     bool flipX = false;
     bool flipY = false;
 
-    if (routeLabel.routeLabelOrientationHorizontal == RouteLabelOrientationHorizontal.left) {
-      if (routeLabel.routeLabelOrientationVertical == RouteLabelOrientationVertical.top) {
+    switch (widget.alignment) {
+      case RouteLabelAlignment.topLeft:
         pointerAlignment = Alignment.topLeft;
-      } else {
+        break;
+      case RouteLabelAlignment.bottomLeft:
         pointerAlignment = Alignment.bottomLeft;
         flipY = true;
-      }
-    } else {
-      if (routeLabel.routeLabelOrientationVertical == RouteLabelOrientationVertical.top) {
+        break;
+      case RouteLabelAlignment.topRight:
         pointerAlignment = Alignment.topRight;
         flipX = true;
-      } else {
+        break;
+      case RouteLabelAlignment.bottomRight:
         pointerAlignment = Alignment.bottomRight;
         flipX = true;
         flipY = true;
-      }
+        break;
     }
 
-    return Stack(children: [
-      Positioned.fill(
-        child: Align(
-          alignment: pointerAlignment,
-          child: Transform.flip(
-            flipX: flipX,
-            flipY: flipY,
-            child: CustomPaint(
-              painter: PointerPainter(
-                context: context,
-                color: routeLabel.selected ? CI.route : CI.secondaryRoute,
-              ),
-              child: const SizedBox(
-                height: cornerIconSize,
-                width: cornerIconSize,
+    bool top = widget.alignment == RouteLabelAlignment.topLeft || widget.alignment == RouteLabelAlignment.topRight;
+    bool bottom =
+        widget.alignment == RouteLabelAlignment.bottomLeft || widget.alignment == RouteLabelAlignment.bottomRight;
+    bool left = widget.alignment == RouteLabelAlignment.topLeft || widget.alignment == RouteLabelAlignment.bottomLeft;
+    bool right =
+        widget.alignment == RouteLabelAlignment.topRight || widget.alignment == RouteLabelAlignment.bottomRight;
+
+    animateOpacity();
+
+    return AnimatedOpacity(
+      opacity: widget.isMapMoving ? 0 : opacity,
+      duration: const Duration(milliseconds: 150),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Align(
+              alignment: pointerAlignment,
+              child: Transform.flip(
+                flipX: flipX,
+                flipY: flipY,
+                child: CustomPaint(
+                  painter: PointerPainter(
+                    context: context,
+                    color: selected ? CI.route : CI.secondaryRoute,
+                  ),
+                  child: const SizedBox(
+                    height: cornerIconSize,
+                    width: cornerIconSize,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
-      Container(
-        margin: EdgeInsets.only(
-          left:
-              routeLabel.routeLabelOrientationHorizontal == RouteLabelOrientationHorizontal.left ? cornerIconMargin : 0,
-          top: routeLabel.routeLabelOrientationVertical == RouteLabelOrientationVertical.top ? cornerIconMargin : 0,
-          right: routeLabel.routeLabelOrientationHorizontal == RouteLabelOrientationHorizontal.right
-              ? cornerIconMargin
-              : 0,
-          bottom:
-              routeLabel.routeLabelOrientationVertical == RouteLabelOrientationVertical.bottom ? cornerIconMargin : 0,
-        ),
-        decoration: BoxDecoration(
-          color: routeLabel.selected ? CI.route : CI.secondaryRoute,
-          borderRadius: BorderRadius.circular(7.5),
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            BoldSmall(
-              text: routeLabel.timeText,
-              context: context,
-              textAlign: TextAlign.center,
-              color: routeLabel.selected ? Colors.white : Colors.black,
+          Container(
+            margin: EdgeInsets.only(
+              left: left ? cornerIconMargin : 0,
+              top: top ? cornerIconMargin : 0,
+              right: right ? cornerIconMargin : 0,
+              bottom: bottom ? cornerIconMargin : 0,
             ),
-            if (routeLabel.secondaryText != null)
-              Small(
-                text: routeLabel.secondaryText!,
-                context: context,
-                textAlign: TextAlign.center,
-                color: routeLabel.selected ? Colors.white : Colors.black,
-              ),
-          ],
-        ),
+            decoration: BoxDecoration(
+              color: selected ? CI.route : CI.secondaryRoute,
+              borderRadius: BorderRadius.circular(7.5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                BoldSmall(
+                  text: timeText,
+                  context: context,
+                  textAlign: TextAlign.center,
+                  color: selected ? Colors.white : Colors.black,
+                ),
+                if (secondaryText != null)
+                  Small(
+                    text: secondaryText!,
+                    context: context,
+                    textAlign: TextAlign.center,
+                    color: selected ? Colors.white : Colors.black,
+                  ),
+              ],
+            ),
+          ),
+        ],
       ),
-    ]);
+    );
   }
 }
 
