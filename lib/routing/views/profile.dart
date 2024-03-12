8fbce86e5897e2f2069fab26aae0aa0a8278e118
adeveloper-wq <dajesch@gmail.com>
2024-03-12 16:34:34 +0100

    Make bottom sheet draggable

--- lib/routing/views/profile.dart
+++ lib/routing/views/profile.dart
@@ -4,8 +4,8 @@ import 'package:priobike/common/layout/spacing.dart';
 import 'package:priobike/common/layout/text.dart';
 import 'package:priobike/common/layout/tiles.dart';
 import 'package:priobike/home/models/profile.dart';
-import 'package:priobike/routing/services/profile.dart';
 import 'package:priobike/main.dart';
+import 'package:priobike/routing/services/profile.dart';
 import 'package:priobike/routing/services/routing.dart';
 
 class ProfileSelectionSheet extends StatefulWidget {
@@ -31,95 +31,94 @@ class ProfileSelectionSheetState extends State<ProfileSelectionSheet> {
     profileService = getIt<Profile>();
     profileService.addListener(update);
     routingService = getIt<Routing>();
-    routingService.addListener(update);
   }
 
   @override
   void dispose() {
     profileService.removeListener(update);
-    routingService.removeListener(update);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.75,
-      child: ListView(
-        padding: const EdgeInsets.fromLTRB(8, 8, 8, 64),
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(26, 16, 26, 0),
-            child: BoldContent(
-              text: "Fahrradtyp auswählen",
-              context: context,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(26, 8, 26, 8),
-            child: Small(
-              text: "Dein gewählter Fahrradtyp beeinflusst das Routing. Starke Anstiege werden immer vermieden.",
-              context: context,
-            ),
-          ),
-          for (var index = 0; index < BikeType.values.length; index++)
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 0, 8, 64),
+        child: Column(
+          children: [
             Padding(
-              padding: const EdgeInsets.all(8),
-              child: Tile(
-                fill: BikeType.values[index] == profileService.bikeType
-                    ? Theme.of(context).colorScheme.primary
-                    : Theme.of(context).colorScheme.background,
-                onPressed: () {
-                  final newBikeType = BikeType.values[index];
-                  profileService.bikeType = newBikeType;
-                  profileService.store();
-                  routingService.loadRoutes();
-                  // Close the modal.
-                  Navigator.of(context).pop();
-                },
-                content: Row(
-                  children: [
-                    Flexible(
-                      fit: FlexFit.tight,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          BoldContent(
-                            text: BikeType.values[index].description(),
-                            context: context,
-                            color: BikeType.values[index] == profileService.bikeType
-                                ? Theme.of(context).colorScheme.onPrimary
-                                : Theme.of(context).colorScheme.onBackground,
-                          ),
-                          const SizedBox(height: 2),
-                          Small(
-                            text: BikeType.values[index].explanation,
-                            context: context,
-                            color: BikeType.values[index] == profileService.bikeType
-                                ? Theme.of(context).colorScheme.onPrimary
-                                : Theme.of(context).colorScheme.onBackground,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    SizedBox(
-                      width: 32,
-                      height: 32,
-                      child: Image(
-                        image: AssetImage(BikeType.values[index].iconAsString()),
-                        color: BikeType.values[index] == profileService.bikeType
-                            ? Theme.of(context).colorScheme.onPrimary
-                            : Theme.of(context).colorScheme.onBackground,
-                        fit: BoxFit.scaleDown,
-                        alignment: Alignment.center,
-                      ),
-                    ),
-                  ],
-                ),
+              padding: const EdgeInsets.fromLTRB(26, 16, 26, 0),
+              child: BoldContent(
+                text: "Fahrradtyp auswählen",
+                context: context,
               ),
-            )
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(26, 8, 26, 8),
+              child: Small(
+                text: "Dein gewählter Fahrradtyp beeinflusst das Routing. Starke Anstiege werden immer vermieden.",
+                context: context,
+              ),
+            ),
+            for (var index = 0; index < BikeType.values.length; index++)
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Tile(
+                  fill: BikeType.values[index] == profileService.bikeType
+                      ? Theme.of(context).colorScheme.primary
+                      : Theme.of(context).colorScheme.background,
+                  onPressed: () {
+                    final newBikeType = BikeType.values[index];
+                    profileService.bikeType = newBikeType;
+                    profileService.store();
+                    routingService.loadRoutes();
+                    // Close the modal.
+                    Navigator.of(context).pop();
+                  },
+                  content: Row(
+                    children: [
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            BoldContent(
+                              text: BikeType.values[index].description(),
+                              context: context,
+                              color: BikeType.values[index] == profileService.bikeType
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Theme.of(context).colorScheme.onBackground,
+                            ),
+                            const SizedBox(height: 2),
+                            Small(
+                              text: BikeType.values[index].explanation,
+                              context: context,
+                              color: BikeType.values[index] == profileService.bikeType
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Theme.of(context).colorScheme.onBackground,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SizedBox(
+                        width: 32,
+                        height: 32,
+                        child: Image(
+                          image: AssetImage(BikeType.values[index].iconAsString()),
+                          color: BikeType.values[index] == profileService.bikeType
+                              ? Theme.of(context).colorScheme.onPrimary
+                              : Theme.of(context).colorScheme.onBackground,
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
@@ -155,6 +154,8 @@ class ProfileButtonState extends State<ProfileButton> {
   void showSelectionDialog() {
     showAppSheet(
       context: context,
+      showDragHandle: true,
+      isScrollControlled: true,
       builder: (BuildContext context) {
         return const ProfileSelectionSheet();
       },
