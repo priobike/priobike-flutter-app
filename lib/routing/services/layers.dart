b5e024c5a3caa30402b6bd7b8302d56ddc76d747
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-04-05 16:29:53 +0200

    Revert "Remove layer showing all construction sites"
    This reverts commit 7a993be165e4c7e9518e1a0e87a3c92591afc01f.

--- lib/routing/services/layers.dart
+++ lib/routing/services/layers.dart
@@ -11,6 +11,9 @@ class Layers with ChangeNotifier {
   /// If parking stations are currently visible.
   bool showParkingStations;
 
+  /// If construction sites are currently visible.
+  bool showConstructionSites;
+
   /// If air stations are currently visible.
   bool showAirStations;
 
@@ -43,6 +46,11 @@ class Layers with ChangeNotifier {
 
   Future<void> setShowParkingStations(bool showParkingStations) async {
     this.showParkingStations = showParkingStations;
+    await storePreferences();
+  }
+
+  Future<void> setShowConstructionSites(bool showConstructionSites) async {
+    this.showConstructionSites = showConstructionSites;
     await storePreferences();
   }
 
@@ -79,6 +87,7 @@ class Layers with ChangeNotifier {
   Layers({
     this.showRentalStations = false,
     this.showParkingStations = false,
+    this.showConstructionSites = false,
     this.showAirStations = false,
     this.showRepairStations = false,
     this.showAccidentHotspots = true,
@@ -100,6 +109,7 @@ class Layers with ChangeNotifier {
 
       showRentalStations = storage.getBool("priobike.layers.showRentalStations") ?? false;
       showParkingStations = storage.getBool("priobike.layers.showParkingStations") ?? false;
+      showConstructionSites = storage.getBool("priobike.layers.showConstructionSites") ?? true;
       showAirStations = storage.getBool("priobike.layers.showAirStations") ?? false;
       showRepairStations = storage.getBool("priobike.layers.showRepairStations") ?? false;
       showAccidentHotspots = storage.getBool("priobike.layers.showAccidentHotspots") ?? false;
@@ -116,6 +126,7 @@ class Layers with ChangeNotifier {
 
     await storage.setBool("priobike.layers.showRentalStations", showRentalStations);
     await storage.setBool("priobike.layers.showParkingStations", showParkingStations);
+    await storage.setBool("priobike.layers.showConstructionSites", showConstructionSites);
     await storage.setBool("priobike.layers.showAirStations", showAirStations);
     await storage.setBool("priobike.layers.showRepairStations", showRepairStations);
     await storage.setBool("priobike.layers.showAccidentHotspots", showAccidentHotspots);
--- lib/routing/views/map.dart
+++ lib/routing/views/map.dart
@@ -187,6 +187,7 @@ class RoutingMapViewState extends State<RoutingMapView> with TickerProviderState
     OfflineCrossingsLayer.layerId,
     TrafficLightsLayer.layerId,
     AccidentHotspotsLayer.layerId,
+    ConstructionSitesLayer.layerId,
     GreenWaveLayer.layerId,
     BikeShopLayer.layerId,
     BikeShopLayer.textLayerId,
@@ -459,6 +460,14 @@ class RoutingMapViewState extends State<RoutingMapView> with TickerProviderState
     } else {
       if (!mounted) return;
       await BikeAirStationLayer.remove(mapController!);
+    }
+    if (layers.showConstructionSites) {
+      final index = await getIndex(ConstructionSitesLayer.layerId);
+      if (!mounted) return;
+      await ConstructionSitesLayer(isDark).install(mapController!, at: index);
+    } else {
+      if (!mounted) return;
+      await ConstructionSitesLayer.remove(mapController!);
     }
     if (layers.showParkingStations) {
       final index = await getIndex(ParkingStationsLayer.layerId);
