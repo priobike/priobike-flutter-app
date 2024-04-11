42de5584701b426060818d52313ebbe2b1ad7f38
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-04-11 16:16:48 +0200

    Fix route label margin

--- lib/routing/views/map.dart
+++ lib/routing/views/map.dart
@@ -755,8 +755,8 @@ class RoutingMapViewState extends State<RoutingMapView> with TickerProviderState
 
     final frame = MediaQuery.of(context);
     // Calculate the relative margin for the route label.
-    const routeLabelMarginLeft = 168.0;
-    const routeLabelMarginRight = 168.0;
+    const routeLabelMarginLeft = 86.0;
+    final routeLabelMarginRight = frame.size.width - 86.0;
     final routeLabelMarginTop = frame.padding.top;
     // Fit initial bottom sheet size of 128px.
     final routeLabelMarginBottom = frame.size.height - 140;
