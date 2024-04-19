79d19a9ffa53eb3a002151ebfdd0374391728e7e
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-04-19 10:37:43 +0200

    Add new straight line mockpositionsource

--- lib/settings/models/positioning.dart
+++ lib/settings/models/positioning.dart
@@ -9,6 +9,7 @@ enum PositioningMode {
   hamburgStatic1,
   dresdenStatic1,
   dresdenStatic2,
+  straightLine,
 }
 
 extension PositioningDescription on PositioningMode {
@@ -34,6 +35,8 @@ extension PositioningDescription on PositioningMode {
         return "Statisch (Dresden, POT vor Ampel 1)";
       case PositioningMode.dresdenStatic2:
         return "Statisch (Dresden, POT vor Ampel 2)";
+      case PositioningMode.straightLine:
+        return "Luftlinie durch Hamburg";
     }
   }
 }
