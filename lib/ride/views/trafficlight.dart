ae8a029b3abce9caddcb438ff2d6fa4c942db4b1
adeveloper-wq <dajesch@gmail.com>
2024-06-19 16:04:53 +0200

    Refactor traffic lights with inside speedometer and use timer for countdown updates

--- lib/ride/views/trafficlight.dart
+++ lib/ride/views/trafficlight.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:priobike/common/layout/text.dart';
 import 'package:priobike/main.dart';
@@ -15,6 +17,94 @@ class RideTrafficLightView extends StatefulWidget {
 }
 
 class RideTrafficLightViewState extends State<RideTrafficLightView> {
+  /// The associated ride service, which is injected by the provider.
+  late Ride ride;
+
+  /// Whether the countdown should be displayed.
+  bool showCountdown = false;
+
+  /// The current phase of the traffic light.
+  Phase? currentPhase;
+
+  /// The time of the phase change.
+  DateTime? phaseChangeTime;
+
+  /// Called when a listener callback of a ChangeNotifier is fired.
+  void update() {
+    var updateView = false;
+
+    // Check if the countdown needs to be updated with new values.
+    final recommendation = ride.predictionProvider!.recommendation!;
+    final newPhase = recommendation.calcCurrentSignalPhase;
+    if (newPhase != currentPhase) {
+      currentPhase = newPhase;
+      updateView = true;
+    }
+    final newPhaseChangeTime = recommendation.calcCurrentPhaseChangeTime;
+    if (newPhaseChangeTime != phaseChangeTime) {
+      phaseChangeTime = newPhaseChangeTime;
+      updateView = true;
+    }
+
+    // Check if the countdown should be displayed/hidden.
+    // Only display the countdown if the distance to the next crossing is less than 500 meters and the prediction quality is good.
+    // Also display the countdown if the user has selected a crossing.
+    var showCountdownNew = (ride.calcDistanceToNextSG ?? double.infinity) < 500;
+    showCountdownNew =
+        showCountdownNew && (ride.predictionProvider?.prediction?.predictionQuality ?? 0) > Ride.qualityThreshold;
+    showCountdownNew = ride.userSelectedSG != null ? true : showCountdownNew;
+    if (showCountdownNew != showCountdown) {
+      showCountdown = showCountdownNew;
+      updateView = true;
+    }
+
+    if (updateView) setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    ride = getIt<Ride>();
+    ride.addListener(update);
+  }
+
+  @override
+  void dispose() {
+    ride.removeListener(update);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedCrossFade(
+      firstCurve: Curves.easeInOutCubic,
+      secondCurve: Curves.easeInOutCubic,
+      sizeCurve: Curves.easeInOutCubic,
+      duration: const Duration(milliseconds: 500),
+      firstChild: CountdownView(
+        size: widget.size,
+        currentPhase: currentPhase!,
+        phaseChangeTime: phaseChangeTime!,
+      ),
+      secondChild: TrafficLightAlternativeInfoView(
+        size: widget.size,
+      ),
+      crossFadeState: showCountdown ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+    );
+  }
+}
+
+class TrafficLightAlternativeInfoView extends StatefulWidget {
+  /// The size of the speedometer.
+  final Size size;
+
+  const TrafficLightAlternativeInfoView({super.key, required this.size});
+
+  @override
+  State<StatefulWidget> createState() => TrafficLightAlternativeInfoViewState();
+}
+
+class TrafficLightAlternativeInfoViewState extends State<TrafficLightAlternativeInfoView> {
   /// The associated ride service, which is injected by the provider.
   late Ride ride;
 
@@ -90,27 +180,64 @@ class RideTrafficLightViewState extends State<RideTrafficLightView> {
       return alternativeView("Ampel in \n$distance");
     }
 
+    return alternativeView("");
+  }
+}
+
+class CountdownView extends StatefulWidget {
+  /// The size of the speedometer.
+  final Size size;
+
+  /// The current phase of the traffic light.
+  final Phase currentPhase;
+
+  /// The time of the phase change.
+  final DateTime phaseChangeTime;
+
+  const CountdownView({super.key, required this.size, required this.currentPhase, required this.phaseChangeTime});
+
+  @override
+  State<StatefulWidget> createState() => CountdownViewState();
+}
+
+class CountdownViewState extends State<CountdownView> {
+  Timer? countdownTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (mounted) setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    countdownTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // Calculate the countdown.
-    final countdown = recommendation.calcCurrentPhaseChangeTime!.difference(DateTime.now()).inSeconds;
+    final countdown = widget.phaseChangeTime.difference(DateTime.now()).inSeconds;
     // If the countdown is 0 (or negative), we hide the countdown. In this way the user
     // is not confused if the countdown is at 0 for a few seconds.
     var countdownLabel = countdown > 5 ? "$countdown" : "";
     // Show no countdown label for amber and redamber.
-    if (recommendation.calcCurrentSignalPhase == Phase.amber) countdownLabel = "";
-    if (recommendation.calcCurrentSignalPhase == Phase.redAmber) countdownLabel = "";
+    if (widget.currentPhase == Phase.amber) countdownLabel = "";
+    if (widget.currentPhase == Phase.redAmber) countdownLabel = "";
 
-    final currentPhase = recommendation.calcCurrentSignalPhase;
-
-    final trafficLight = Container(
+    return Container(
       width: widget.size.width * 0.6,
       height: widget.size.width * 0.6,
       decoration: BoxDecoration(
         gradient: RadialGradient(
           stops: const [0.2, 0.8, 1],
           colors: [
-            currentPhase.color,
-            currentPhase.color.withOpacity(0.2),
-            currentPhase.color.withOpacity(0),
+            widget.currentPhase.color,
+            widget.currentPhase.color.withOpacity(0.2),
+            widget.currentPhase.color.withOpacity(0),
           ],
         ),
         borderRadius: BorderRadius.circular(64),
@@ -161,23 +288,6 @@ class RideTrafficLightViewState extends State<RideTrafficLightView> {
           ),
         ],
       ),
-    );
-
-    // Only display the countdown if the distance to the next crossing is less than 500 meters and the prediction quality is good.
-    // Also display the countdown if the user has selected a crossing.
-    var showCountdown = (ride.calcDistanceToNextSG ?? double.infinity) < 500;
-    showCountdown =
-        showCountdown && (ride.predictionProvider?.prediction?.predictionQuality ?? 0) > Ride.qualityThreshold;
-    showCountdown = ride.userSelectedSG != null ? true : showCountdown;
-
-    return AnimatedCrossFade(
-      firstCurve: Curves.easeInOutCubic,
-      secondCurve: Curves.easeInOutCubic,
-      sizeCurve: Curves.easeInOutCubic,
-      duration: const Duration(milliseconds: 500),
-      firstChild: trafficLight,
-      secondChild: alternativeView(""),
-      crossFadeState: showCountdown ? CrossFadeState.showFirst : CrossFadeState.showSecond,
     );
   }
 }
