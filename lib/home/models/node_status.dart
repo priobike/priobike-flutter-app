8fc634c3ae9c0f8cb9363ec24efa7b1f06f43c35
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-06-21 12:05:06 +0200

    Add recommendFallback to model

--- lib/home/models/node_status.dart
+++ lib/home/models/node_status.dart
@@ -1,18 +1,23 @@
 class NodeStatus {
-  /// The current load status of the ingress node.
+  /// If the user is recommended to use the fallback backend.
+  final bool recommendFallback;
+
+  /// If the node has a warning.
   final bool warning;
 
   /// The timestamp of the current load status data.
   final DateTime timestamp;
 
-  NodeStatus({required this.warning, required this.timestamp});
+  NodeStatus({required this.recommendFallback, required this.warning, required this.timestamp});
 
   factory NodeStatus.fromJson(Map<String, dynamic> json) => NodeStatus(
+        recommendFallback: json['recommend_fallback'],
         warning: json['warning'],
         timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp'] * 1000),
       );
 
   Map<String, dynamic> toJson() => {
+        'recommend_fallback': recommendFallback,
         'warning': warning,
         'timestamp': timestamp.millisecondsSinceEpoch,
       };
