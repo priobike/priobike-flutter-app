0e4d00afcff5afb8e26b139eb07dbdd09b6e92bf
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-06-26 10:59:36 +0200

    Add location notification icon

--- lib/positioning/services/positioning.dart
+++ lib/positioning/services/positioning.dart
@@ -236,7 +236,7 @@ class Positioning with ChangeNotifier {
             accuracy: desiredAccuracy,
             distanceFilter: 0,
             foregroundNotificationConfig: const ForegroundNotificationConfig(
-              notificationIcon: AndroidResource(name: 'priobike_foreground', defType: 'drawable'),
+              notificationIcon: AndroidResource(name: 'ic_location', defType: 'drawable'),
               notificationText: "Die Navigation läuft.",
               notificationTitle: "Navigation",
               setOngoing: true,
