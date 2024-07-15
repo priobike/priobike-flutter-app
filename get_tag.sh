0d9dd757ea141b78cf16c3048c9bf75030286953
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-07-15 13:15:23 +0200

    Add echo to test github action

--- get_tag.sh
+++ get_tag.sh
@@ -5,6 +5,8 @@ GIT_TAG=$(git tag --points-at HEAD)
 
 # Check if the git tag contains "release"
 if [[ $GIT_TAG == "release-"* ]]; then
+  # To check in ci script
+  echo $GIT_TAG
   # Write the tag to a text file
   echo $GIT_TAG > git_tag.txt
 fi
