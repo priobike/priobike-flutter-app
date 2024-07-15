f33d7c30defe8fe1c71fec106aea0fe5fd15a830
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-07-15 13:56:07 +0200

    Use checkout v4 for release action

--- get_tag.sh
+++ get_tag.sh
@@ -3,10 +3,11 @@
 # Fetch the latest Git tag
 GIT_TAG=$(git tag --points-at HEAD)
 
+# To check in ci script
+echo $GIT_TAG
+
 # Check if the git tag contains "release"
 if [[ $GIT_TAG == "release-"* ]]; then
-  # To check in ci script
-  echo $GIT_TAG
   # Write the tag to a text file
   echo $GIT_TAG > git_tag.txt
 fi
