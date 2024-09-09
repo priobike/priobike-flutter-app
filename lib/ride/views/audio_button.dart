113c6f02e19bac54fa0114125052217843305f9e
adeveloper-wq <dajesch@gmail.com>
2024-09-09 16:35:37 +0200

    Add toast and hint that audio is deactivated when the user selects a SG manually

--- lib/ride/views/audio_button.dart
+++ lib/ride/views/audio_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:priobike/common/layout/tiles.dart';
+import 'package:priobike/logging/toast.dart';
 import 'package:priobike/main.dart';
+import 'package:priobike/ride/services/ride.dart';
 import 'package:priobike/settings/services/settings.dart';
 
 class AudioButton extends StatefulWidget {
@@ -14,6 +16,9 @@ class _AudioButtonState extends State<AudioButton> {
   /// The associated settings service, which is injected by the provider.
   late Settings settings;
 
+  /// The associated ride service, which is incjected by the provider.
+  late Ride ride;
+
   /// Called when a listener callback of a ChangeNotifier is fired.
   void update() => setState(() {});
 
@@ -23,11 +28,15 @@ class _AudioButtonState extends State<AudioButton> {
 
     settings = getIt<Settings>();
     settings.addListener(update);
+
+    ride = getIt<Ride>();
+    ride.addListener(update);
   }
 
   @override
   void dispose() {
     settings.removeListener(update);
+    ride.removeListener(update);
 
     super.dispose();
   }
@@ -47,19 +56,29 @@ class _AudioButtonState extends State<AudioButton> {
           width: 58,
           height: 58,
           child: Tile(
-            onPressed: () => settings.setAudioInstructionsEnabled(!settings.audioSpeedAdvisoryInstructionsEnabled),
+            onPressed: () {
+              if (ride.userSelectedSG == null) {
+                settings.setAudioInstructionsEnabled(!settings.audioSpeedAdvisoryInstructionsEnabled);
+              } else {
+                getIt<Toast>().showError("Zentrieren, um Audio zu aktivieren");
+              }
+            },
             padding: const EdgeInsets.all(10),
             fill: Theme.of(context).colorScheme.surfaceVariant,
             content: settings.audioSpeedAdvisoryInstructionsEnabled
                 ? Icon(
                     Icons.volume_up,
                     size: 32,
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    color: ride.userSelectedSG != null
+                        ? Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.2)
+                        : Theme.of(context).colorScheme.onSurfaceVariant,
                   )
                 : Icon(
                     Icons.volume_off,
                     size: 32,
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    color: ride.userSelectedSG != null
+                        ? Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.2)
+                        : Theme.of(context).colorScheme.onSurfaceVariant,
                   ),
           ),
         ),
